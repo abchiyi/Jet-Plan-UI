743bc73435eb6c806c5497f08eb5c125bbe794bb
abchiyi <meme_me2019@outlook.com>
2021-08-02 10:16:44 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+    pages: {
+        index: {
+            template: 'public/index.html',
+            entry: 'src/main.js',
+            filename: 'index.html'
+        }
+    },
+    // 为packages目录添加babel-loader处理
+    chainWebpack: config => {
+        config.module
+            .rule('js')
+            .include
+            .add(resolve('packages'))
+            .end()
+            .use('babel')
+            .loader('babel-loader')
+            .tap(options => {
+                return options
+            })
+    }
+}
