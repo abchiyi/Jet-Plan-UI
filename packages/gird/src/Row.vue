8f7454e5734e28a45b257cfa99d9c009c88bfc6a
abchiyi <meme_me2019@outlook.com>
2021-08-04 00:59:42 +0800

    ...

--- packages/gird/src/Row.vue
+++ packages/gird/src/Row.vue
@@ -1,11 +1,46 @@
 <script>
+import { h } from "vue";
 export default {
   name: "m-row",
-  render(h) {
-    return h("div", this.$slots);
+  props: {
+    start: {
+      type: String,
+      default: "start",
+        validator: v => {
+          const value = ["start", "center", "end"];
+          return value.indexOf(v.toLowerCase()) !== -1;
+        },
+    },
+    spaceMode: {
+      type: String,
+      default: undefined,
+      validator: v => {
+        const value = ["around", "between"];
+        return value.indexOf(v.toLowerCase()) !== -1;
+      },
+    },
+  },
+  render() {
+    return h(
+      "div",
+      {
+        class: ["m-row"],
+        style: {
+          // 设置文本横向起始位置
+          justifyContent: this.start,
+        },
+      },
+      this.$slots
+    );
   },
 };
 </script>
 
-<style>
+<style scoped>
+.m-row {
+  justify-content: flex-start;
+  flex-direction: row;
+  display: flex;
+  width: 100%;
+}
 </style>
