86d467ef987e734ce4b8329fb79c4d2314a3a558
abchiyi <meme_me2019@outlook.com>
2021-08-07 23:13:27 +0800

    添加链接

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link>|
+    <router-link to="/girdedemo">GirdeDemo</router-link>
   </div>
   <router-view />
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <m-row
-      style="height: 300px; border: 1px solid black; border-radius: 20px"
-      Y="center"
-      reverse
-    >
-      <h1>Hello world</h1>
-      <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-      <div class="col">1</div>
-      <div class="col">2</div>
-      <div class="col">3</div>
-    </m-row>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    // HelloWorld,
-  },
+    HelloWorld
+  }
 };
 </script>
