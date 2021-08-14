3c764660f254f4503c7d2e88a017cb4ba0d9ef86
meme—me2019 <meme_me2019@outlook.com>
2021-08-15 05:51:52 +0800

    添加分割符

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>|
-    <router-link to="/girdedemo">GirdeDemo</router-link>
-    <router-link to="/animationdemo">AnimationDemo</router-link>
+    <router-link to="/girdedemo">GirdeDemo</router-link>|
+    <router-link to="/animationdemo">AnimationDemo</router-link>|
   </div>
   <router-view />
 </template>
