3232013b9d0cc3895870a2c602ea27e58afa11ed
meme <meme_me2019@outlook.com>
2021-08-27 05:04:50 +0800

    删除不必要样式

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,6 @@ window.onresize = function() {
   res = setTimeout(function() {
     console.log("resize triggered");
   }, 20);
-
-  console.log(123);
 };
 export default {
   name: "App",
@@ -30,24 +28,10 @@ export default {
 
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exsact-active {
-  color: #42b983;
-}
+} */
 </style>
