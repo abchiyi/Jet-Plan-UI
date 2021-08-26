76a03f13a58843f348a03718daf168b89e77e786
meme <meme_me2019@outlook.com>
2021-08-27 05:05:19 +0800

    原生组件样式设置

--- packages/theme_default/nativeComponents/index.js
+++ packages/theme_default/nativeComponents/index.js
@@ -1,7 +1,9 @@
 import button from './src/css/button.css'
+import header from './src/css/header.css'
 import body from './src/css/body.css'
 import hr from './src/css/hr.css'
 export default {
+    header,
     button,
     body,
     hr,
