1300ae5576672c42b4f1ca3687828cd84fca9fde
meme <meme_me2019@outlook.com>
2021-08-27 05:23:23 +0800

    迁移样式

--- packages/form/src/Button.vue
+++ packages/form/src/Button.vue
@@ -65,3 +65,107 @@ export default {
   }
 };
 </script>
+<style scoped>
+/* TODO  迟滞的动画效果 */
+/*------------ Default ----------*/
+
+.m-button {
+  text-decoration: underline 2px;
+  background-color: var(--bgco-0);
+  display: inline-block;
+  color: var(--grey-4);
+  padding: 6px 16px;
+  font-weight: bold;
+  user-select: none;
+  font-size: 14px;
+  margin: 1px 2px;
+}
+
+/*------------ Primary ----------*/
+
+.m-button.button.primary {
+  background-color: var(--main-0);
+  color: var(--grey-0);
+}
+
+/*------------ Active ----------*/
+.m-button.button:active {
+  filter: brightness(0.85);
+}
+
+/*------------ Animation ----------*/
+
+.m-button {
+  transition: filter 0.1s cubic-bezier(0.2, 1, 0.5, 1), all 0.3s ease-out;
+}
+
+/*------------ disabled ----------*/
+
+.m-button.disabled:active,
+.m-button.disabled {
+  color: var(--grey-2);
+  cursor: not-allowed;
+  filter: unset;
+}
+
+/*------------ Disabled Primary  ----------*/
+.m-button.button.primary.disabled:active,
+.m-button.button.primary.disabled {
+  background-color: var(--bgco-1);
+  color: var(--grey-0);
+}
+
+/*------------ Radius ----------*/
+
+.m-button.button.radius-sm {
+  border-radius: 4px;
+}
+
+.m-button.button.radius-xl {
+  border-radius: 1rem;
+}
+
+/*------------ OneLine ----------*/
+.m-button.row {
+  display: block;
+  width: 100%;
+}
+
+/* Text Button */
+
+/*------------ Default ----------*/
+
+.m-button.text-button {
+  background: unset;
+  color: var(--grey-4);
+  transition: opacity 0.2s cubic-bezier(0.2, 1, 0.5, 1);
+  padding: unset;
+}
+
+/*------------ Active ----------*/
+
+.m-button.text-button:active {
+  opacity: 0.6;
+}
+
+/*------------ Primary ----------*/
+
+.m-button.text-button.primary {
+  color: var(--main-0);
+}
+
+/* ------------ disabled ---------- */
+
+.m-button.text-button.disabled:active,
+.m-button.text-button.disabled {
+  color: var(--grey-2);
+}
+
+/*------------ Disabled Primary  ----------*/
+
+.m-button.text-button.primary.disabled:active,
+.m-button.text-button.primary.disabled {
+  color: var(--bgco-1);
+  opacity: unset;
+}
+</style>
