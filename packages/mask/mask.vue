7f2af3ae3d66779c7fdc82d36cc60d570484709d
meme <meme_me2019@outlook.com>
2021-08-27 12:02:07 +0800

    暂存

---
+++ packages/mask/mask.vue
@@ -0,0 +1,27 @@
+<template>
+  <fade>
+    <div class="mask"></div>
+  </fade>
+</template>
+
+<script>
+import fade from "../../packages/animations/src/TransitionFade.vue";
+export default {
+  name: "Mask",
+  components: {
+    fade
+  }
+};
+</script>
+
+<style>
+.mask {
+  background-color: var(--shadow);
+  opacity: 0.3;
+  position: absolute;
+  height: 120%;
+  width: 120%;
+  top: -10%;
+  left: -10%;
+}
+</style>
--- src/views/the_demo/TheTest.vue
+++ src/views/the_demo/TheTest.vue
@@ -1,10 +1,49 @@
 <template>
-  <h2>This is testing page</h2>
+  <div>
+    <h2>This is testing page</h2>
+    <m-row
+      X="center"
+      Y="center"
+      @mousedown="reValue"
+      @mouseup="reValue"
+      @touchstart="reValue"
+      @touchend="reValue"
+      class="test-box"
+    >
+      <mmm :value="value"></mmm>
+      <h3>Push down:</h3>
+      <h3>Mask {{ value ? "Active" : "Disabled" }}</h3>
+    </m-row>
+  </div>
 </template>
 
 <script>
-export default {};
+import mmm from "../../../packages/mask/mask.vue";
+export default {
+  name: "TheTestPage",
+  components: {
+    mmm
+  },
+  data() {
+    return {
+      value: false
+    };
+  },
+  methods: {
+    reValue() {
+      this.value = !this.value;
+      console.log(this.value);
+    }
+  }
+};
 </script>
 
 <style>
+.test-box {
+  height: 300px;
+  border-radius: 16px;
+  border: solid 4px var(--grey);
+  position: relative;
+  overflow: hidden;
+}
 </style>
