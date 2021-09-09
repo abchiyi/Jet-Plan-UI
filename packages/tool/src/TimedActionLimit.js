88dbcb4ef34962d67b6428ec482bbf033589349d
meme <meme_me2019@outlook.com>
2021-09-09 20:50:43 +0800

    超时执行次数限制模块

---
+++ packages/tool/src/TimedActionLimit.js
@@ -0,0 +1,30 @@
+export default class TimedActionLimit {
+    constructor(timeout, limit = 1) {
+        this.limit = limit;
+        this.timeout = timeout;
+        this.timeoutID = [];
+    }
+
+    action(callback) {
+        const timeoutObj = setTimeout(
+            () => {
+                callback()
+            }, this.timeout
+        )
+        this.timeoutID.push(timeoutObj)
+
+        // 移除超时的动作
+        setTimeout(
+            () => {
+                let index = this.timeoutID.indexOf(timeoutObj)
+                if (index != -1) this.timeoutID.splice(index, 1)
+            }, this.timeout
+        )
+
+        // 多次时间内执行动作,取消执行先前动作
+        if (this.timeoutID.length > this.limit) {
+            let id = this.timeoutID.shift()
+            clearTimeout(id)
+        }
+    }
+}
