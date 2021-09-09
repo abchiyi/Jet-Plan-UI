a17b95f86aea3046c20e4139c5ffae65868e32ea
meme <meme_me2019@outlook.com>
2021-09-09 20:51:26 +0800

    检测屏幕 尺寸模块

---
+++ packages/tool/src/ReScreenSize.js
@@ -0,0 +1,29 @@
+import ta from "./TimedActionLimit";
+const ReSize = function () {
+    let width = window.innerWidth;
+    return {
+        width: width,
+        col: {
+            xs: width < 575,
+            sm: width > 576 && width < 768,
+            md: width > 768 && width < 992,
+            lg: width > 992 && width < 1200,
+            xl: width > 1200
+        },
+    }
+}
+
+const timeout = new ta(200)
+export default {
+    name: 're-screen-size',
+    created() {
+        window.addEventListener('resize', () => {
+            const binding = arguments[1]
+            timeout.action(() => {
+                binding.value(
+                    ReSize()
+                )
+            })
+        })
+    }
+}
