83e32abd3a0e19e9828cf51c691f26f11129ffb3
meme <meme_me2019@outlook.com>
2021-09-10 23:30:32 +0800

    修改超时等待为100ms,修改输出值,添加当前屏幕宽度状态

--- packages/tool/directives/src/ReScreenSize.js
+++ packages/tool/directives/src/ReScreenSize.js
@@ -1,19 +1,26 @@
 import ta from "../../src/TimedActionLimit";
 const ReSize = function () {
     let width = window.innerWidth;
+    let col = {
+        xs: width < 576,
+        sm: width > 576 && width < 768,
+        md: width > 768 && width < 992,
+        lg: width > 992 && width < 1200,
+        xl: width > 1200
+    }
+    function activeCol() {
+        for (let i in col) {
+            if (col[i]) return i
+        }
+    }
     return {
+        activeCol: activeCol(),
         width: width,
-        col: {
-            xs: width < 575,
-            sm: width > 576 && width < 768,
-            md: width > 768 && width < 992,
-            lg: width > 992 && width < 1200,
-            xl: width > 1200
-        },
+        col: col,
     }
 }
 
-const timeout = new ta(200)
+const timeout = new ta(100)
 export default {
     name: 're-screen-size',
     created() {
