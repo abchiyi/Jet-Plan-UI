dd8fa4a5348437a6e9e4f7e6e1352b45ae1c0f77
meme <meme_me2019@outlook.com>
2021-11-23 00:06:54 +0800

    删除页面点击反馈

--- src/views/the_demo/TheDemoButtons.vue
+++ src/views/the_demo/TheDemoButtons.vue
@@ -4,20 +4,19 @@
     <p>m-button 基本按钮组件</p>
 
     <h2>基本示例</h2>
-    <m-click-feedback>
-      <m-demo-box>
-        <m-row>
-          <m-button></m-button>
-        </m-row>
-      </m-demo-box>
-    </m-click-feedback>
+    <m-demo-box>
+      <m-row>
+        <m-button></m-button>
+        <m-button text></m-button>
+      </m-row>
+    </m-demo-box>
   </div>
 </template>
 
 <script>
 export default {
   name: "the-demo-buttons",
-  component: {}
+  component: {},
 };
 </script>
 
