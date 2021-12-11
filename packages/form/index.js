5aa486811641da28258efef09817ba7a5edad75c
meme <meme_me2019@outlook.com>
2021-12-11 12:08:49 +0800

    添加组件引用

--- packages/form/index.js
+++ packages/form/index.js
@@ -1,15 +1,18 @@
 import Button from './src/Button.vue'
 import Switch from './src/Switch.vue'
+import Input from './src/Input.vue'
 import './src/css/shape.css'
 
 export {
     Button,
     Switch,
+    Input
 }
 
 export default {
     install(Vue) {
         Vue.component(Button.name, Button)
         Vue.component(Switch.name, Switch)
+        Vue.component(Input.name, Input)
     }
 }
