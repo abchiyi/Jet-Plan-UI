2e66a28b42544195305e2aa5d5f8be9b0ed7edf7
meme <meme_me2019@outlook.com>
2021-12-13 20:00:02 +0800

    格式化

--- packages/theme/index.js
+++ packages/theme/index.js
@@ -1,22 +1,22 @@
 function getThemeStyleEl(elId) {
-  let styleEl = document.getElementById(elId);
-  if (styleEl) return styleEl;
-  styleEl = document.createElement('style');
-  styleEl.id = elId;
-  document.head.appendChild(styleEl)
-  return styleEl
+    let styleEl = document.getElementById(elId);
+    if (styleEl) return styleEl;
+    styleEl = document.createElement('style');
+    styleEl.id = elId;
+    document.head.appendChild(styleEl)
+    return styleEl
 }
 
 
 export default {
-  use(theme) {
-    const themeEl = getThemeStyleEl('test-color')
-    const innerHTML = [];
-    for (const key in theme) {
-      if (Object.hasOwnProperty.call(theme, key)) {
-        innerHTML.push(`--${key}:${theme[key]}`)
-      }
+    use(theme) {
+        const themeEl = getThemeStyleEl('test-color')
+        const innerHTML = [];
+        for (const key in theme) {
+            if (Object.hasOwnProperty.call(theme, key)) {
+                innerHTML.push(`--${key}:${theme[key]}`)
+            }
+        }
+        themeEl.innerHTML = `:root{${innerHTML.join(';')}}`
     }
-    themeEl.innerHTML = `:root{${innerHTML.join(';')}}`
-  }
 }
