05168d6242a17996eb6e7bf0eb2170e51f347911
meme <meme_me2019@outlook.com>
2021-12-13 21:38:18 +0800

    [change]代码结构调整

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import router from './router'
 import App from './App.vue'
 
 import common from './common'
-
 import ui from '@'
 
 ui.theme.use(ui.themeDefault.colors.Light)
-const app = createApp(App)
-app.use(router)
-app.use(ui)
-app.use(common)
-app.mount('#app')
+
+createApp(App)
+    .use(common)
+    .use(router)
+    .use(ui)
+    .mount('#app')
