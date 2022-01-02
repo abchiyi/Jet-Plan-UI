0f65eb4554abd7ab50d62141709f79e0cc34d87e
meme <meme_me2019@outlook.com>
2022-01-02 11:14:50 +0800

    [Remove]删除冗余代码

--- packages/card/src/Card.vue
+++ packages/card/src/Card.vue
@@ -6,8 +6,8 @@ export default {
     zDepth: {
       // TODD 限制范围
       type: Number,
-      default: 2,
-    },
+      default: 2
+    }
   },
   computed: {
     classes() {
@@ -16,28 +16,25 @@ export default {
     },
     styles() {
       return {
-        "--z-depth": this.zDepth * 0.1,
+        "--z-depth": this.zDepth * 0.1
       };
-    },
+    }
   },
   render() {
     return h(
       "div",
       {
         class: this.classes,
-        style: this.styles,
+        style: this.styles
       },
       this.$slots
     );
-  },
+  }
 };
 </script>
 
 <style>
 .m-card {
-  /* TODO 可调的阴影等级 */
-  /* box-shadow: 0 1px 3px 1px rgba(46, 47, 59, var(--z-depth)); */
-  border-radius: var(--s-radius);
   box-shadow: var(--shadow);
   background: var(--card);
   overflow: hidden;
