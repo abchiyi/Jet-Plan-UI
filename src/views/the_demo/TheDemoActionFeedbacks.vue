efceb4acbe8d8cc72227d8d9e23b2d06f7a5f479
meme <meme_me2019@outlook.com>
2022-01-16 03:47:57 +0800

    --

--- src/views/the_demo/TheDemoActionFeedbacks.vue
+++ src/views/the_demo/TheDemoActionFeedbacks.vue
@@ -1,18 +1,10 @@
 <template>
   <h2>动作反馈</h2>
-  <m-click-feedback class="r">
-    123
-  </m-click-feedback>
-  <div style="height:300px;border:solid 1px green;overflow:hidden">
-    <div class="r" ref="r" @click="log"></div>
-  </div>
-
-  <demo-box title="淡出淡入" :code="code" id="fade">
-    <m-button activeAnimation>asddddddddddd</m-button>
+  <demo-box title="点击波纹反馈" :code="code" id="fade">
     <m-row class="animation-box" X="center">
       <m-click-feedback>
         <m-cube v-show="value">
-          <p>Fade</p>
+          <p>ClickMe</p>
         </m-cube>
       </m-click-feedback>
     </m-row>
@@ -20,50 +12,22 @@
 </template>
 
 <script>
-function aa(el) {
-  const box = el.getBoundingClientRect();
-  //   const body = document.body;
-  //   const clientTop = el.clientTop || body.clientTop || 0;
-  //   const clientLeft = el.clientLeft || body.clientLeft || 0;
-  const clientTop = el.clientTop || 0;
-  const clientLeft = el.clientLeft || 0;
-  const scrollTop = window.pageYOffset || el.scrollTop;
-  const scrollLeft = window.pageXOffset || el.scrollLeft;
-  return {
-    top: box.top + scrollTop - clientTop,
-    left: box.left + scrollLeft - clientLeft
-  };
-}
-import { getOffset } from "../../../packages/tool/src/dom";
 export default {
-  name: "animation-fade",
+  name: "the-demo-action-feedbacks",
   data() {
     return {
       value: true,
       code: ``
     };
-  },
-  methods: {
-    log() {
-      let el = this.$refs.r;
-      console.log(getOffset(el));
-      console.log(aa(el));
-    }
   }
 };
 </script>
 
 <style scoped >
 .m-cube {
-  border: 4px solid var(--border);
+  border: 3px solid var(--border);
   background: var(--foreground);
   box-sizing: border-box;
   color: var(--text);
-}
-.r {
-  height: 400px;
-  width: 400px;
-  background: rgba(255, 255, 255, 0.102);
-  /* border: solid 20px red; */
 }
 </style>
