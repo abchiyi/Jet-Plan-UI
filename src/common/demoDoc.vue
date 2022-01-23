aa9042dc597ce57e15bdfaf677d743e8733e09df
meme <meme_me2019@outlook.com>
2022-01-23 11:30:11 +0800

    [Add] Doc 组件

---
+++ src/common/demoDoc.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- Doc -->
+  <article class="prop">
+    <strong>Props:</strong>
+    <slot name="prop">
+      <ul class="doc">
+        <li>无任何接口。</li>
+      </ul>
+    </slot>
+  </article>
+  <article class="slot">
+    <strong>Props:</strong>
+    <slot name="slot">
+      <ul class="doc">
+        <li>无插槽，任何组件或HTML&文本都将被忽略</li>
+      </ul>
+    </slot>
+  </article>
+  <article class="Event">
+    <strong>Event:</strong>
+    <slot name="event">
+      <ul class="event">
+        <li>仅有原生事件</li>
+      </ul>
+    </slot>
+  </article>
+</template>
+<script>
+export default {
+  name: "demo-doc"
+};
+</script>
+
+<style>
+.doc li + li {
+  margin-top: 1em;
+}
+
+.doc li::marker {
+  margin: unset;
+}
+</style>
