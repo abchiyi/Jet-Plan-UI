57d97b41a73f989e9ae7e91361289fc57433f8c1
meme <meme_me2019@outlook.com>
2022-02-04 04:32:51 +0800

    [Change]添加保留字 直接返回顶部

--- packages/tool/directives/ScrollTo.js
+++ packages/tool/directives/ScrollTo.js
@@ -5,24 +5,30 @@ import {
 } from '../src/dom'
 
 function scroll(elID, toTop) {
-    const SCREENHEIGHT = getOffset($('body')[0]).size.height
-    const ELHEIGHT = getOffset($(elID)[0]).size.height
-    const TOP = $(elID).offset().top
-
-    // 设置 toTop 的默认值
-    toTop = toTop ? Math.round(toTop) : 200
-
     let position;
-    if (ELHEIGHT <= SCREENHEIGHT) {
-        // 当元素接近屏幕高度时
-        position = ELHEIGHT >= SCREENHEIGHT * 0.75 ?
-            TOP - toTop :
-            TOP - (SCREENHEIGHT - ELHEIGHT) / 2
+    if (elID.toLowerCase() === "top") {
+        // 保留字段,返回顶部
+        position = 0
     } else {
-        position = TOP - toTop
+        const SCREENHEIGHT = getOffset($('body')[0]).size.height
+        const ELHEIGHT = getOffset($(elID)[0]).size.height
+        const TOP = $(elID).offset().top
+
+        // 设置 toTop 的默认值
+        toTop = toTop ? Math.round(toTop) : 200
+
+        if (ELHEIGHT <= SCREENHEIGHT) {
+            // 当元素接近屏幕高度时
+            position = ELHEIGHT >= SCREENHEIGHT * 0.75 ?
+                TOP - toTop :
+                TOP - (SCREENHEIGHT - ELHEIGHT) / 2
+        } else {
+            position = TOP - toTop
+        }
+
+        position = position < 1 ? 0 : position // 元素在顶部时,设置为0
     }
 
-    position = position < 1 ? 0 : position // 元素在顶部时,设置为0
 
     $('html, body').animate({
         scrollTop: position
