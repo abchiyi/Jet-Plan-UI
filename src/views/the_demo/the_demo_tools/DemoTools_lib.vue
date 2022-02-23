d30790b6f286ee4755a06278aeb8be2c8df91c3b
meme <meme_me2019@outlook.com>
2022-02-24 05:21:31 +0800

    [Change]demo  doc update

--- src/views/the_demo/the_demo_tools/DemoTools_lib.vue
+++ src/views/the_demo/the_demo_tools/DemoTools_lib.vue
@@ -9,33 +9,96 @@
       <h2>Class
         <inline-code class="">TimedActionLimit</inline-code>
       </h2>
+
+      <p>
+        <inline-code>TimedActionLimit</inline-code> 作用为限制单位时间内所能执行操作的次数。
+      </p>
+      <p>在下方 Demo 中限制为 2s内可执行2次操作</p>
+      <!-- Demo -->
       <demo-box
-        title='Class TimedActionLimit'
+        title='TimedActionLimit'
         :code='code'
       >
-        <m-cube
-          id="demo-show-click-conter"
-          @click="click"
-        >
-          {{ clickConter}}
-        </m-cube>
+        <div>
+          <!-- 冷却进度条 -->
+          <div
+            :class="cooldownProgressClass"
+            id="demo-cooldown-progress"
+          >
+            <m-row X="center">{{text}}</m-row>
+          </div>
+          <!-- 点击示例 -->
+          <m-cube
+            id="demo-show-click-conter"
+            @click="click"
+            :style="{fontSize:clickConter==0?'1em':''}"
+          >
+            {{ clickConter == 0? 'ClickMe!!':clickConter}}
+          </m-cube>
+        </div>
       </demo-box>
+      <doc-of-class></doc-of-class>
     </article>
   </div>
 </template>
 <script>
 import inlineCode from '../../../common/inlineCode.vue';
 import { TimedActionLimit } from '@/tool/lib'
-const tal = new TimedActionLimit(1000);
-
+const tal = new TimedActionLimit(2000, 2);
 export default {
   components: { inlineCode },
   name: 'the-tool-lib',
   data: () => {
     return {
-      //TODO示例代码
-      code: undefined,
-      clickConter: 0
+      code: `//注意！code 示例为 demo 的简化版本
+//JavaScript
+let clickConter = 0;
+const tal = new TimedActionLimit(2000,2);
+
+tal.setCooledAlarm(() => {
+    alert('Cooled!')
+    })
+tal.setOverheatAlarm(() => {
+    alert('Overheat!')
+    })
+
+function click() {
+    tal.action(()=>{
+        clickConter++;
+    })
+}
+
+//HTML
+<body>
+    <p onclick="click()">{{ clickConter == 0? 'ClickMe!!':clickConter }}</p>
+</body>
+      `,
+      clickConter: 0,
+      overheat: false,
+      text: "Cooled!"
+    }
+  },
+  mounted () {
+    tal.setCooledAlarm(
+      () => {
+        this.text = 'Cooled!'
+      }
+    )
+    tal.setOverheatAlarm(
+      () => {
+        this.overheat = true
+        this.text = 'Overheat!'
+      }
+    )
+  },
+  watch: {
+    overheat (v) {
+      if (v) {
+        setTimeout(() => {
+          this.overheat = false
+          this.text = 'Cooling...'
+        }, 1000)
+      }
     }
   },
   methods: {
@@ -44,6 +107,14 @@ export default {
         this.clickConter++
       })
     }
+  },
+  computed: {
+    cooldownProgressClass () {
+      return [
+        this.overheat ? 'overheat' : '',
+        !this.overheat && this.clickConter ? 'cooling' : ''
+      ]
+    }
   }
 };
 </script>
@@ -51,5 +122,40 @@ export default {
 #demo-show-click-conter {
   font-family: SarasaMono;
   font-size: 48px;
+}
+#demo-cooldown-progress {
+  background-color: var(--success);
+  border-radius: var(--s-radius);
+  font-family: SarasaMono;
+  display: inline-block;
+  margin-bottom: 1em;
+  padding: 5px;
+  height: 1em;
+  width: 90px;
+}
+
+#demo-cooldown-progress.overheat {
+  transition: all 0.5s var(--ease-out);
+  background-color: var(--error);
+  width: 100%;
+}
+
+#demo-cooldown-progress.cooling {
+  transition: background 0.2s var(--ease-out);
+  animation: cooling 1s var(--ease-out);
+}
+
+@keyframes cooling {
+  0% {
+    background-color: var(--error);
+    width: 100%;
+  }
+  80% {
+    background-color: var(--warning);
+  }
+  100% {
+    background-color: var(--success);
+    width: 90px;
+  }
 }
 </style>
