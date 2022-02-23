51d72bfb3ef6bb4cd1d7c5841c06d3038f078028
meme <meme_me2019@outlook.com>
2022-02-24 03:53:43 +0800

    [Change]添加外部通知功能

--- packages/tool/lib/index.js
+++ packages/tool/lib/index.js
@@ -5,17 +5,22 @@ export class TimedActionLimit {
         this.timeoutID = [];
         this.overheat = false
         this.conter = limit
+        this.cooledAlarm = false
+        this.overheatAlarm = false
     }
 
     action (callback) {
-        console.log(this);
         if (!this.overheat) {
             callback()
             this.conter = this.conter - 1
             this.overheat = this.conter == 0
         }
+
         if (this.overheat) {
             // 未冷却状态再次执行动作，重置冷却时间
+
+            // 通知外部程序已过热
+            if (this.overheatAlarm) this.overheatAlarm()
 
             // 清除冷却动作
             this.timeoutID.forEach((timeoutID) => {
@@ -27,9 +32,19 @@ export class TimedActionLimit {
             let timeoutID = setTimeout(() => {
                 this.overheat = false
                 this.conter = this.limit
+                // 通知外部程序已冷却
+                if (this.cooledAlarm) this.cooledAlarm()
             }, this.timeout)
             this.timeoutID.push(timeoutID)
         }
+    }
+
+    setCooledAlarm (callback) {
+        this.cooledAlarm = callback
+    }
+
+    setOverheatAlarm (callback) {
+        this.overheatAlarm = callback
     }
 }
 
