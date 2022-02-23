c9a37d004053fb096f42b5afe35aed6e6cc66be0
meme <meme_me2019@outlook.com>
2022-02-24 01:57:49 +0800

    [Add] Demo tools

--- src/views/the_demo/the_demo_tools/DemoTools.vue
+++ src/views/the_demo/the_demo_tools/DemoTools.vue
@@ -2,12 +2,19 @@
   <div>
     <h2>工具</h2>
     <p>这里是一些内置的工具函数&类，以及一些指令的函数版本。</p>
-
+    <lib></lib>
+    <dom></dom>
   </div>
 </template>
 <script>
+import Lib from './DemoTools_lib.vue'
+import Dom from './DemoTools_lib_dom.vue'
 export default {
   name: 'the-demo-tools',
+  components: {
+    Lib,
+    Dom,
+  },
   data: () => {
     return {
     }
---
+++ src/views/the_demo/the_demo_tools/DemoTools_lib.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="too-lib">
+    <p>
+      工具存放以下路径中
+      <inline-code>tool/lib</inline-code>
+    </p>
+    <!--功能函数 -->
+    <article id='time-action-limit'>
+      <h2>Class
+        <inline-code class="">TimedActionLimit</inline-code>
+      </h2>
+      <demo-box
+        title=''
+        :code='code'
+      >
+      </demo-box>
+    </article>
+
+  </div>
+</template>
+<script>
+import inlineCode from '../../../common/inlineCode.vue';
+export default {
+  components: { inlineCode },
+  name: 'the-tool-lib',
+  data: () => {
+    return {
+      //TODO示例代码
+      code: undefined
+    }
+  }
+};
+</script>
+<style>
+</style>
