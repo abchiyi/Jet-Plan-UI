7e46ce77d79a8d8f5828cf06f7c502a3d51bd2b3
meme <meme_me2019@outlook.com>
2022-02-24 02:40:42 +0800

    [Change] demo update

--- src/views/the_demo/the_demo_tools/DemoTools_lib.vue
+++ src/views/the_demo/the_demo_tools/DemoTools_lib.vue
@@ -4,32 +4,52 @@
       工具存放以下路径中
       <inline-code>tool/lib</inline-code>
     </p>
-    <!--功能函数 -->
+    <!--TimeActionLimit -->
     <article id='time-action-limit'>
       <h2>Class
         <inline-code class="">TimedActionLimit</inline-code>
       </h2>
       <demo-box
-        title=''
+        title='Class TimedActionLimit'
         :code='code'
       >
+        <m-cube
+          id="demo-show-click-conter"
+          @click="click"
+        >
+          {{ clickConter}}
+        </m-cube>
       </demo-box>
     </article>
-
   </div>
 </template>
 <script>
 import inlineCode from '../../../common/inlineCode.vue';
+import { TimedActionLimit } from '@/tool/lib'
+const tal = new TimedActionLimit(1000);
+
 export default {
   components: { inlineCode },
   name: 'the-tool-lib',
   data: () => {
     return {
       //TODO示例代码
-      code: undefined
+      code: undefined,
+      clickConter: 0
+    }
+  },
+  methods: {
+    click () {
+      tal.action(() => {
+        this.clickConter++
+      })
     }
   }
 };
 </script>
 <style>
+#demo-show-click-conter {
+  font-family: SarasaMono;
+  font-size: 48px;
+}
 </style>
