4da3566d5371ef2383e1ae27ee602b283f7dbd94
meme <meme_me2019@outlook.com>
2022-02-24 03:03:43 +0800

    [Change]更新 TimeActionLimit 算法

--- packages/tool/lib/index.js
+++ packages/tool/lib/index.js
@@ -3,28 +3,32 @@ export class TimedActionLimit {
         this.limit = limit;
         this.timeout = timeout;
         this.timeoutID = [];
+        this.overheat = false
+        this.conter = limit
     }
 
     action (callback) {
-        const timeoutObj = setTimeout(
-            () => {
-                callback()
-            }, this.timeout
-        )
-        this.timeoutID.push(timeoutObj)
+        console.log(this);
+        if (!this.overheat) {
+            callback()
+            this.conter = this.conter - 1
+            this.overheat = this.conter == 0
+        }
+        if (this.overheat) {
+            // 未冷却状态再次执行动作，重置冷却时间
 
-        // 移除超时的动作
-        setTimeout(
-            () => {
-                let index = this.timeoutID.indexOf(timeoutObj)
-                if (index != -1) this.timeoutID.splice(index, 1)
-            }, this.timeout
-        )
+            // 清除冷却动作
+            this.timeoutID.forEach((timeoutID) => {
+                clearTimeout(timeoutID)
+            })
+            this.timeoutID = []
 
-        // 多次时间内执行动作,取消执行先前动作
-        if (this.timeoutID.length > this.limit) {
-            let id = this.timeoutID.shift()
-            clearTimeout(id)
+            // 设置冷却动作
+            let timeoutID = setTimeout(() => {
+                this.overheat = false
+                this.conter = this.limit
+            }, this.timeout)
+            this.timeoutID.push(timeoutID)
         }
     }
 }
