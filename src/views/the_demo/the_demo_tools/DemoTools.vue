c0b098b3ec95b62a21fbfdab4eee8dee0e6a05bb
meme <meme_me2019@outlook.com>
2022-02-27 14:50:26 +0800

    [Change]文件名迁移

--- src/views/the_demo/the_demo_tools/DemoTools.vue
+++ src/views/the_demo/the_demo_tools/DemoTools.vue
@@ -2,18 +2,15 @@
   <div>
     <h2>工具</h2>
     <p>这里是一些内置的工具函数&类，以及一些指令的函数版本。</p>
-    <lib></lib>
-    <dom></dom>
+    <demo-tal />
   </div>
 </template>
 <script>
-import Lib from './DemoTools_lib.vue'
-import Dom from './DemoTools_lib_dom.vue'
+import demoTal from './DemoTimedActionLimit.vue'
 export default {
   name: 'the-demo-tools',
   components: {
-    Lib,
-    Dom,
+    demoTal
   },
   data: () => {
     return {
