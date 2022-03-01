1b12c76b8d27ee55e8c9c6d1da3c1f144dce1f66
meme <meme_me2019@outlook.com>
2022-03-02 02:33:00 +0800

    [Add] ++

---
+++ src/common/DocItme.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="doc">
+    <strong>{{name}}：</strong>
+    <ul>
+      <slot>
+        <li style="color:var(--error);"><strong>Empty !</strong></li>
+      </slot>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "doc-item",
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+  }
+};
+</script>
+
+<style>
+.dov {
+  margin: 10px;
+}
+
+.doc li + li {
+  margin-top: 1em;
+}
+
+.doc li::marker {
+  margin: unset;
+}
+
+.doc + .doc {
+  margin-bottom: 20px;
+}
+
+.doc.no-dot > ol,
+.doc.no-dot > ul {
+  list-style-type: none;
+}
+</style>
