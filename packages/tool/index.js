e4d4be3f5a43f82ed59dfe54141e7005b98fe387
meme <meme_me2019@outlook.com>
2022-03-03 10:19:54 +0800

    [Change] tool 导出更改

---
+++ packages/tool/index.js
@@ -0,0 +1,14 @@
+import { TimedActionLimit } from './lib';
+import {
+    SHADOW_PAINTER_ERROR,
+    shadowPainter,
+    scrollTo,
+} from './lib/dom'
+
+
+export {
+    TimedActionLimit,
+    SHADOW_PAINTER_ERROR,
+    shadowPainter,
+    scrollTo,
+}
