39aebe2cdc2076b8ef35fd624fd51fa2984a8457
meme <meme_me2019@outlook.com>
2022-03-03 10:21:17 +0800

    [Change]针对动画的状态切换限制测

--- src/views/the_demo/TheTest.vue
+++ src/views/the_demo/TheTest.vue
@@ -1,77 +1,32 @@
 <template>
-  <div>
-    <m-row X="center">
-      <m-cube
-        id="div1"
-        style="background: var(--primary); height: 900px"
-      >
-        <strong>Primary</strong>
-      </m-cube>
-    </m-row>
-    <m-row X="center">
-      <m-cube
-        id="div2"
-        style="background: var(--warning); height: 200px"
-      >
-        <strong>Waring</strong>
-      </m-cube>
-    </m-row>
-    <m-row X="center">
-      <m-cube
-        id="div3"
-        style="background: var(--error); height: 900px"
-      >
-        <strong>Error</strong>
-      </m-cube>
-    </m-row>
-    <m-row X="center">
-      <m-cube
-        id="div4"
-        style="background: var(--info); height: 600px"
-      >
-        <strong>Innfo</strong>
-      </m-cube>
-    </m-row>
-  </div>
-
+  <m-button @click="()=>{ tal.action(()=>{value = !value})}">
+  </m-button>
+  <m-transition-slide
+    :opacity="true/false"
+    position="bottom"
+  >
+    <div
+      class="deom-item"
+      v-show="value"
+    >
+      <m-cube>To top</m-cube>
+    </div>
+  </m-transition-slide>
 </template>
 <script>
 import pdn from '../../common/mix/popDemoName'
+import { TimedActionLimit } from '../../../packages/tool'
 export default {
   mixins: [pdn],
   name: "TheTestPage",
   data () {
     return {
-      checkboxCheckAll: true,
-      checkboxData: ["c1", "c2", "c3", "c4"],
-      sd: ["top", "bottom", "left", "bottom"],
-      checkbox: [],
-      value1: false,
-      value: false,
-      //   text:
-      //     '<m-switch size="s" v-model="value1"></m-switch>\n<m-switch size="m" disabled v-model="value1"></m-switch>\n<m-switch size="l"v-model="value1"></m-switch>'
-      text: "1",
+      value: true,
+      tal: new TimedActionLimit(510, 1, false)
     };
   },
   methods: {
-    on () {
-      this.value = true;
-      this.text = 5;
-    },
-    off () {
-      this.value = false;
-      this.text = 0;
-    },
-    test () {
-      this.value = !this.value;
-    },
-    hal () {
-      return this.checkboxData.length;
-    },
   },
-  watch: {},
-  created () { },
-  mounted () { },
 };
 </script>
 
