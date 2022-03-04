37c8ef165aa04fe8fbe0f97db16fa58f20eac59d
meme <meme_me2019@outlook.com>
2022-03-04 19:51:55 +0800

    [Change]格式化

--- src/common/DocItme.vue
+++ src/common/DocItme.vue
@@ -1,62 +1,63 @@
 <template>
-  <div :class="classes">
-    <component :is="titleIs">
-      {{name}}
-    </component>
-    <ul>
-      <slot>
-        <li style="color:var(--error);"><strong>Empty !</strong></li>
-      </slot>
-    </ul>
-  </div>
+	<div :class="classes">
+		<component :is="titleIs">
+			{{ name }}
+		</component>
+		<ul>
+			<slot>
+				<li style="color: var(--error)"><strong>Empty !</strong></li>
+			</slot>
+		</ul>
+	</div>
 </template>
 <script>
-export default {
-  name: "doc-item",
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    titleIs: {
-      type: String,
-      default: 'strong'
-    },
-    noDot: {
-      type: Boolean,
-      default: false
-    },
-  },
-  computed: {
-    classes () {
-      return [
-        'doc',
-        this.noDot ? "no-dot" : ""
-      ]
-    }
-  }
-};
+	export default {
+		name: 'doc-item',
+		props: {
+			name: {
+				type: String,
+				required: true,
+			},
+			titleIs: {
+				type: String,
+				default: 'strong',
+			},
+			noDot: {
+				type: Boolean,
+				default: false,
+			},
+		},
+		computed: {
+			classes() {
+				return ['doc', this.noDot ? 'no-dot' : ''];
+			},
+		},
+	};
 </script>
 
 <style>
-.dov {
-  margin: 10px;
-}
+	.dov {
+		margin: 10px;
+	}
 
-.doc li + li {
-  margin-top: 1em;
-}
+	.doc li + li {
+		margin-top: 1em;
+	}
 
-.doc li::marker {
-  margin: unset;
-}
+	.doc li::marker {
+		margin: unset;
+	}
 
-.doc + .doc {
-  margin-bottom: 20px;
-}
+	.doc + .doc {
+		margin-bottom: 20px;
+	}
 
-.doc.no-dot > ol,
-.doc.no-dot > ul {
-  list-style-type: none;
-}
+	.doc.no-dot > ol,
+	.doc.no-dot > ul {
+		list-style-type: none;
+	}
+
+	.doc + .doc {
+		margin-top: 3em;
+	}
 </style>
