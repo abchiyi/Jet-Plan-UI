35d58607dfa68657c4dcc01fbc0bea0f71b9776c
meme <meme_me2019@outlook.com>
2022-03-04 23:54:16 +0800

    [Add] todo

--- packages/code_box/src/CodeBox.vue
+++ packages/code_box/src/CodeBox.vue
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+	// TODO hljs 主题跟随 ui
 	import 'highlight.js/styles/atom-one-dark.css';
 	import 'highlight.js/lib/common';
 	import hljsVuePlugin from '@highlightjs/vue-plugin';
