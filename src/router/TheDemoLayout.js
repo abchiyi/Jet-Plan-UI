f5c88ed6eb1020fd40b85e50c813174ae7f21a11
meme <meme_me2019@outlook.com>
2022-03-04 19:51:45 +0800

    [Change]样式修正

--- src/router/TheDemoLayout.js
+++ src/router/TheDemoLayout.js
@@ -2,6 +2,6 @@ export default [
     {
         path: "/gride",
         name: "Gride",
-        component: () => import('../views/the_demo/the_demo_layout/TheDemoGirde.vue')
+        component: () => import('../views/the_demo/the_demo_layout/TheDemoGirde.vue'),
     },
 ]
