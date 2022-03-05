8063a4123c1df852315a19d6d415b68f9948980c
meme <meme_me2019@outlook.com>
2022-03-06 03:09:02 +0800

    [Add]添加 Scale 过渡

--- packages/animations/index.js
+++ packages/animations/index.js
@@ -1,16 +1,19 @@
 import TransitionFolded from './src/TransitionFolded.vue'
 import TransitionSlide from './src/TransitionSlide.vue'
 import TransitionFade from './src/TransitionFade.vue'
+import TransitionScale from './src/TransitionScale'
 export {
     TransitionFolded,
     TransitionSlide,
     TransitionFade,
+    TransitionScale,
 }
 
 export default {
-    install(Vue) {
+    install (Vue) {
         Vue.component(TransitionFolded.name, TransitionFolded)
         Vue.component(TransitionSlide.name, TransitionSlide)
         Vue.component(TransitionFade.name, TransitionFade)
+        Vue.component(TransitionScale.name, TransitionScale)
     }
 }
---
+++ packages/animations/src/TransitionScale.vue
@@ -0,0 +1,30 @@
+<script>
+	import { h, TransitionGroup } from 'vue';
+	export default {
+		name: 'm-transition-scale',
+		render() {
+			return h(
+				TransitionGroup,
+				{
+					name: 'm-transition-scale',
+					mode: 'out-in',
+				},
+				{
+					default: () => this.$slots.default(),
+				}
+			);
+		},
+	};
+</script>
+
+<style>
+	.m-transition-scale-enter-active,
+	.m-transition-scale-leave-active {
+		transition: all 0.4s var(--ease-out);
+	}
+	.m-transition-scale-enter-from,
+	.m-transition-scale-leave-active {
+		transform: scale(0) !important;
+		opacity: 0;
+	}
+</style>
