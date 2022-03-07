3a59decd38fb43c884fa1dfd05cdfdbed18ef314
meme <meme_me2019@outlook.com>
2022-03-07 12:21:23 +0800

    [Add] doc add

--- src/router/TheDemoLayout.js
+++ src/router/TheDemoLayout.js
@@ -4,4 +4,9 @@ export default [
         name: "Gride",
         component: () => import('../views/the_demo/the_demo_layout/TheDemoGirde.vue'),
     },
+    {
+        path: "/control-bar",
+        name: "ControlBar",
+        component: () => import('../views/the_demo/the_demo_layout/TheDemoControlBar.vue'),
+    },
 ]
---
+++ src/views/the_demo/the_demo_layout/TheDemoControlBar.vue
@@ -0,0 +1,179 @@
+<template>
+	<div id="demo-m-control-bar">
+		<doc-itme name="<m-control-bar> :" title-is="h2">
+			<li id="m-control-bar-props">
+				<doc-itme name="Props:" title-is="h3">
+					<li id="prop-for-id">
+						<p>
+							<inline-code>for-id</inline-code>
+							type - String, required - true
+						</p>
+						<p>
+							被控制组件的
+							<inline-code>id</inline-code>
+							,这个值是必须的
+						</p>
+					</li>
+					<li id="prop-reverse">
+						<p>
+							<inline-code>reverse</inline-code>
+							type - Boolean, default - false
+						</p>
+						<p>反向排列内部元素</p>
+					</li>
+				</doc-itme>
+			</li>
+			<li id="m-control-bar-slots">
+				<doc-itme name="Slots:" title-is="h3">
+					<li id="slot-text">
+						<p>
+							<inline-code>text</inline-code>
+							文本或任意你想要放置于内部的元素
+						</p>
+					</li>
+					<li id="slot-control">
+						<p>
+							<inline-code>control</inline-code>
+							只能放置 input 组件以及 input 衍生组件
+						</p>
+					</li>
+				</doc-itme>
+			</li>
+		</doc-itme>
+		<doc-itme name="如何使用">
+			<li id="demo">
+				<demo-box title="<m-control-bar>" :code="code" :expand="true">
+					<template v-slot:header>
+						<m-control-bar for-id="to-reverse">
+							<template v-slot:text>反向排列元素</template>
+							<template v-slot:control>
+								<m-switch id="to-reverse" v-model="toReverse" />
+							</template>
+						</m-control-bar>
+					</template>
+					<div id="demo-default">
+						<m-control-bar
+							for-id="demo-switch"
+							:reverse="toReverse"
+						>
+							<template v-slot:text>Switch</template>
+							<template v-slot:control>
+								<m-switch id="demo-switch" v-model="value" />
+							</template>
+						</m-control-bar>
+						<m-control-bar
+							for-id="demo-checkbox"
+							:reverse="toReverse"
+						>
+							<template v-slot:text>Checkbox</template>
+							<template v-slot:control>
+								<m-checkbox
+									id="demo-checkbox"
+									v-model="value"
+								/>
+							</template>
+						</m-control-bar>
+						<m-control-bar
+							for-id="demo-radio1"
+							:reverse="toReverse"
+						>
+							<template v-slot:text>Radio1</template>
+							<template v-slot:control>
+								<m-radio
+									id="demo-radio1"
+									value="demo-value1"
+									v-model="valueRadio"
+								/>
+							</template>
+						</m-control-bar>
+						<m-control-bar
+							for-id="demo-radio2"
+							:reverse="toReverse"
+						>
+							<template v-slot:text>Radio2</template>
+							<template v-slot:control>
+								<m-radio
+									id="demo-radio2"
+									value="demo-value2"
+									v-model="valueRadio"
+								/>
+							</template>
+						</m-control-bar>
+					</div>
+				</demo-box>
+			</li>
+		</doc-itme>
+	</div>
+</template>
+<script>
+	import DocItme from '../../../common/DocItme.vue';
+	import InlineCode from '../../../common/inlineCode.vue';
+	export default {
+		components: { DocItme, InlineCode },
+		name: '',
+		data: () => {
+			return {
+				code: `
+//Javascript
+let value = false;
+let valueRadio = [];
+let toReverse = false,
+
+
+// HTML
+<div id="demo-default">
+<m-control-bar
+    for-id="demo-switch"
+    :reverse="toReverse"
+>
+    <template v-slot:text>Switch</template>
+    <template v-slot:control>
+        <m-switch id="demo-switch" v-model="value" />
+    </template>
+</m-control-bar>
+<m-control-bar
+    for-id="demo-checkbox"
+    :reverse="toReverse"
+>
+    <template v-slot:text>Checkbox</template>
+    <template v-slot:control>
+        <m-checkbox
+            id="demo-checkbox"
+            v-model="value"
+        />
+    </template>
+</m-control-bar>
+<m-control-bar
+    for-id="demo-radio1"
+    :reverse="toReverse"
+>
+    <template v-slot:text>Radio1</template>
+    <template v-slot:control>
+        <m-radio
+            id="demo-radio1"
+            value="demo-value1"
+            v-model="valueRadio"
+        />
+    </template>
+</m-control-bar>
+<m-control-bar
+    for-id="demo-radio2"
+    :reverse="toReverse"
+>
+    <template v-slot:text>Radio2</template>
+    <template v-slot:control>
+        <m-radio
+            id="demo-radio2"
+            value="demo-value2"
+            v-model="valueRadio"
+        />
+    </template>
+</m-control-bar>`,
+				value: false,
+				valueRadio: [],
+				toReverse: false,
+			};
+		},
+	};
+</script>
+<style></style>
