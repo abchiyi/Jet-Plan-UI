2bc3b70200465d55f2980ebc59c7390f096ed5a8
meme <meme_me2019@outlook.com>
2022-03-08 16:37:46 +0800

    [Change] 删除事件触发

--- packages/layout/src/FullScreenMask.vue
+++ packages/layout/src/FullScreenMask.vue
@@ -1,25 +1,21 @@
 <template>
-  <span @click="clicked" class="full-screen-mask"> </span>
+	<span class="full-screen-mask"></span>
 </template>
 
 <script>
-export default {
-  name: "FullScreenMask",
-  methods: {
-    clicked() {
-      this.$emit("click");
-    },
-  },
-};
+	export default {
+		name: 'FullScreenMask',
+	};
 </script>
 
 <style scoped>
-.full-screen-mask {
-  position: fixed;
-  height: 100%;
-  width: 100%;
-  left: 0;
-  top: 0;
-  background-color: --var(--shadow);
-}
+	.full-screen-mask {
+		position: fixed;
+		height: 100%;
+		width: 100%;
+		left: 0;
+		top: 0;
+		background-color: var(--shadow);
+		animation: ;
+	}
 </style>
