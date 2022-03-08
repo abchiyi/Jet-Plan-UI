318823101456ae7fd80391f8c1a42730171da08a
meme <meme_me2019@outlook.com>
2022-03-08 16:07:40 +0800

    [Del]删除不再使用的组件

--- packages/layout/index.js
+++ packages/layout/index.js
@@ -1,18 +1,15 @@
-import Size from './src/Size.js'
 import Page from './src/Page.vue'
 import Header from './Header.vue'
 import Sidebar from './Sidebar.vue'
 import Controlbar from "./src/ControlBar.vue";
 export {
-    Size,
     Header,
     Sidebar,
     Controlbar
 }
 
 export default {
-    Size,
-    install(Vue) {
+    install (Vue) {
         Vue.component(Page.name, Page)
         Vue.component(Header.name, Header)
         Vue.component(Sidebar.name, Sidebar)
