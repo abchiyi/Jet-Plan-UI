fe6f88266688417641e8df97d9bf6c9c04c2fb60
meme <meme_me2019@outlook.com>
2022-03-11 21:03:25 +0800

    [Change] 样式调整

--- src/common/DemoPageHaveIndex.vue
+++ src/common/DemoPageHaveIndex.vue
@@ -1,69 +1,53 @@
-<template >
-  <div class="demo-page-have-index">
-    <ul class="container">
-      <li
-        class="item"
-        v-for="item in paths"
-        :key="item"
-      >
-        <!-- Link -->
-        <router-link-a
-          :href="item.href"
-          style="padding-left: 1.5rem"
-        >
-          {{ item.name }}
-        </router-link-a>
-        <!-- Index -->
-        <m-transition-folded>
-          <div
-            class="page-index"
-            v-show="$route.path == item.href"
-          >
-            <m-button
-              class="item"
-              v-scroll-to:120="i.to"
-              style="padding-left: 2.5rem"
-              v-for="i in item.index"
-              :key="i"
-              active
-              hover
-              text
-              row
-            >
-              {{ i.name }}
-            </m-button>
-          </div>
-        </m-transition-folded>
-      </li>
-    </ul>
-  </div>
+<template>
+	<div class="demo-page-have-index">
+		<ul class="container">
+			<li class="item" v-for="item in paths" :key="item">
+				<!-- Link -->
+				<router-link-a :href="item.href" style="padding-left: 1.5rem">
+					{{ item.name }}
+				</router-link-a>
+				<!-- Index -->
+				<m-transition-folded>
+					<div class="page-index" v-show="$route.path == item.href">
+						<m-button
+							class="item"
+							v-scroll-to:120="i.to"
+							style="padding-left: 2.5rem"
+							v-for="i in item.index"
+							:key="i"
+							active
+							hover
+							text
+							row
+						>
+							{{ i.name }}
+						</m-button>
+					</div>
+				</m-transition-folded>
+			</li>
+		</ul>
+	</div>
 </template>
 <script>
-export default {
-  name: "demo-page-have-index",
-  data () {
-    return {
-      path: undefined
-    }
-  },
-  props: {
-    paths: Object,
-  }
-}
+	export default {
+		name: 'demo-page-have-index',
+		data() {
+			return {
+				path: undefined,
+			};
+		},
+		props: {
+			paths: Object,
+		},
+	};
 </script>
 <style>
-.item::marker {
-  font-size: 0;
-}
+	.item::marker {
+		font-size: 0;
+	}
 
-#links,
-.container {
-  padding: unset;
-}
-
-.item > .router-link-a,
-.page-index > .item {
-  font-size: 15px;
-  padding: 8px 0;
-}
+	#links,
+	.container {
+		padding: unset;
+	}
 </style>
--- src/common/RouterLinkA.vue
+++ src/common/RouterLinkA.vue
@@ -1,46 +1,39 @@
 <template>
-  <m-button
-    text
-    hover
-    row
-    :class="className"
-    @click="click"
-  >
-    <slot />
-  </m-button>
+	<m-button text hover row :class="className" @click="click">
+		<slot />
+	</m-button>
 </template>
 
 <script>
-export default {
-  name: "router-link-a",
-  props: {
-    href: String,
-    active: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  computed: {
-    className () {
-      return ["router-link-a", this.$route.path == this.href ? "active" : ""];
-    },
-  },
-  methods: {
-    click () {
-      if (this.href) {
-        this.$router.push(this.href);
-      }
-    },
-  },
-};
+	export default {
+		name: 'router-link-a',
+		props: {
+			href: String,
+			active: {
+				type: Boolean,
+				default: false,
+			},
+		},
+		computed: {
+			className() {
+				return [
+					'router-link-a',
+					this.$route.path == this.href ? 'active' : '',
+				];
+			},
+		},
+		methods: {
+			click() {
+				if (this.href) {
+					this.$router.push(this.href);
+				}
+			},
+		},
+	};
 </script>
 
 <style>
-.router-link-a {
-  text-align: start;
-  /* margin: 2px 0; */
-}
-.router-link-a.active {
-  color: var(--primary);
-}
+	.router-link-a.active {
+		color: var(--primary);
+	}
 </style>
