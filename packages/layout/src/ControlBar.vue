9eceb9f3530c83b8d4dfee30524aa6ec31950055
meme <meme_me2019@outlook.com>
2022-03-11 22:17:12 +0800

    [Change]样式修正

--- packages/layout/src/ControlBar.vue
+++ packages/layout/src/ControlBar.vue
@@ -1,84 +1,77 @@
 <template>
-  <m-action-feedback
-    class="m-control-bar"
-    active
-  >
-    <m-row
-      :reverse="reverse"
-      space-mode="between"
-    >
-      <m-row
-        class="control-label"
-        :for="forId"
-        tag="label"
-        Y="center"
-        :X="reverse ? 'end' : 'start'"
-        style="text-align:start"
-      >
-        <slot name="text">
-          <p>Display text</p>
-        </slot>
-      </m-row>
-      <m-row
-        :reverse="reverse"
-        class="control-comp"
-        Y="center"
-      >
-        <slot name="control">
-          <p>Control</p>
-        </slot>
-      </m-row>
-    </m-row>
-  </m-action-feedback>
+	<div class="m-control-bar">
+		<m-action-feedback active>
+			<m-row :reverse="reverse" space-mode="between">
+				<m-row
+					class="control-label"
+					:for="forId"
+					tag="label"
+					Y="center"
+					:X="reverse ? 'end' : 'start'"
+					style="text-align: start"
+				>
+					<slot name="text">
+						<p>Display text</p>
+					</slot>
+				</m-row>
+				<m-row :reverse="reverse" class="control-comp" Y="center">
+					<slot name="control">
+						<p>Control</p>
+					</slot>
+				</m-row>
+			</m-row>
+		</m-action-feedback>
+	</div>
 </template>
 
 <script>
-import { Row } from "../../gird/";
-export default {
-  name: "m-control-bar",
-  components: {
-    "m-row": Row
-  },
-  props: {
-    forId: { type: String, required: true },
-    reverse: { type: Boolean, default: false }
-  }
-};
+	import { Row } from '../../gird/';
+	export default {
+		name: 'm-control-bar',
+		components: {
+			'm-row': Row,
+		},
+		props: {
+			forId: { type: String, required: true },
+			reverse: { type: Boolean, default: false },
+		},
+	};
 </script>
 
 <style>
-.control-label {
-  user-select: none;
-}
-.m-control-bar {
-  border-radius: var(--m-radius);
-  background: var(--foreground);
-  font-size: 1rem;
-  display: block;
-  margin: 1rem 0;
-}
+	.control-label {
+		user-select: none;
+	}
+	.m-control-bar {
+		border-radius: var(--m-radius);
+		background: var(--foreground);
+		overflow: hidden;
+		font-size: 1rem;
+		display: block;
+		margin: 1rem 0;
+	}
 
-.control-comp,
-.control-label {
-  height: 2.7rem;
-}
+	.control-comp,
+	.control-label {
+		height: 2.7rem;
+	}
 
-.control-label {
-  margin-left: 10px;
-  width: 100%;
-}
+	.control-label {
+		margin-left: 10px;
+		width: 100%;
+	}
 
-.control-comp {
-  margin-right: 10px;
-}
+	.control-comp {
+		margin-right: 10px;
+	}
 
-.reverse .control-comp {
-  margin-left: 10px;
-  margin-right: unset;
-}
+	.reverse .control-comp {
+		margin-left: 10px;
+		margin-right: unset;
+	}
 
-.reverse .control-label {
-  margin-right: 10px;
-  margin-left: unset;
-}
+	.reverse .control-label {
+		margin-right: 10px;
+		margin-left: unset;
+	}
 </style>
