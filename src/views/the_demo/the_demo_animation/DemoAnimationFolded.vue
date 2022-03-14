2c4bd59f73da6e51a5e7bcc486cd8e6831b9da15
meme <meme_me2019@outlook.com>
2022-03-14 10:38:41 +0800

    [Change]格式化，优化文档结构

--- src/views/the_demo/the_demo_animation/DemoAnimationFolded.vue
+++ src/views/the_demo/the_demo_animation/DemoAnimationFolded.vue
@@ -1,48 +1,49 @@
 <template>
-  <h2>折叠展开</h2>
-  <m-control-bar for-id="re-folded-value">
-    <template v-slot:text>
-      {{ value ? "折叠" : "展开" }}
-    </template>
-    <template v-slot:control>
-      <m-button
-        id="re-folded-value"
-        @click="
-          () => {
-            value = !value;
-          }
-        "
-        >ClickMe</m-button
-      >
-    </template>
-  </m-control-bar>
-  <demo-box title="折叠展开" :code="code" id="folded">
-    <m-row class="animation-box" X="center">
-      <m-transition-folded>
-        <div v-show="value">
-          <m-cube>
-            <p>Folded</p>
-          </m-cube>
-        </div>
-      </m-transition-folded>
-    </m-row>
-  </demo-box>
+	<h2>折叠展开</h2>
+	<m-control-bar for-id="re-folded-value">
+		<template v-slot:text>
+			{{ value ? '折叠' : '展开' }}
+		</template>
+		<template v-slot:control>
+			<m-button
+				id="re-folded-value"
+				@click="
+					() => {
+						value = !value;
+					}
+				"
+			>
+				ClickMe
+			</m-button>
+		</template>
+	</m-control-bar>
+	<demo-box title="折叠展开" :code="code" id="folded">
+		<div>
+			<!-- <m-row X="center"> -->
+			<m-transition-folded>
+				<m-cube v-show="value">
+					<p>Folded</p>
+				</m-cube>
+			</m-transition-folded>
+		</div>
+		<!-- </m-row> -->
+	</demo-box>
 </template>
 
 <script>
-export default {
-  name: "animation-folded",
-  data() {
-    return {
-      value: true,
-      code: `<m-transition-folded>
+	export default {
+		name: 'animation-folded',
+		data() {
+			return {
+				value: true,
+				code: `<m-transition-folded>
     <div v-show="value">
         <m-cube>
         <p>Folded</p>
         </m-cube>
     </div>
-</m-transition-folded>`
-    };
-  }
-};
+</m-transition-folded>`,
+			};
+		},
+	};
 </script>
