dfebc53b6f82a74c4207473cd6d7ae1b8264e7cf
meme <meme_me2019@outlook.com>
2022-03-15 23:14:02 +0800

    [Change]控制逻辑更改

--- packages/tool/directives/ReScreenSize.js
+++ packages/tool/directives/ReScreenSize.js
@@ -32,11 +32,6 @@ function lastScreenUpdate (callback) {
 export default {
     name: 're-screen-size',
     created (el, binding) {
-        // const binding = arguments[1]
-        binding.value(
-            el,
-            ReSize()
-        )
         window.addEventListener('resize', () => {
             timeout.action(() => {
                 binding.value(
@@ -52,6 +47,12 @@ export default {
             }
             )
         })
+    },
+    mounted (el, binding) {
+        binding.value(
+            el,
+            ReSize()
+        )
     },
     reSize: ReSize
 }
