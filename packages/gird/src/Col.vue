8be5c34327a4e193ea2bedc3c34e43e08036ccb7
meme <meme_me2019@outlook.com>
2022-03-17 06:43:26 +0800

    [StyleChange]

--- packages/gird/src/Col.vue
+++ packages/gird/src/Col.vue
@@ -60,7 +60,7 @@
 			style() {
 				return {
 					// col
-					col: this.col ? this.calcWidth(this.col) : '',
+					col: this.col ? this.calcWidth(this.col) : '100%',
 					xs: this.calcWidth(this.xs),
 					sm: this.calcWidth(this.sm),
 					md: this.calcWidth(this.md),
