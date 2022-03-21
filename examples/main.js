0155e1208e30589d89339f43f165bc820ed1c94b
meme <meme_me2019@outlook.com>
2022-03-21 11:45:54 +0800

    [Change] 使用硬链接引用文件

--- examples/main.js
+++ examples/main.js
@@ -5,26 +5,19 @@ import {
     h
 } from 'vue'
 
-// // UI
-// import ui from '@'
-// import {
-//     theme,
-//     themeDefault
-// } from '@'
-
 import ui from 'jet-plan-ui'
-// import { theme, themeDefault } from 'jet-plan-ui'
+import { theme, themeDefault } from 'jet-plan-ui'
 
 // Custom compents
 import router from './router'
 import common from './common'
-// import {
-//     redColorModeCookie,
-// } from './common'
+
+import {
+    redColorModeCookie,
+} from './common'
 
 // Set theme
-// XXX 应设置默认颜色选项
-// theme.use(themeDefault, ...redColorModeCookie())
+theme.use(themeDefault, ...redColorModeCookie())
 
 const BASE_COMPONENT = {
     name: "mousseUI-home-page",
