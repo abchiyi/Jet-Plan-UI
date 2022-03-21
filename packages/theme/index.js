2856e1872355e05c8c429e81a68cf890f1e60ee8
meme <meme_me2019@outlook.com>
2022-03-21 11:44:56 +0800

    [Change] 结构迁移

--- packages/theme/index.js
+++ packages/theme/index.js
@@ -14,40 +14,48 @@ function setTheme (el, colotString) {
 
 }
 
-export default {
-    use (themes, auto, darkMode) {
-        const EL = getThemeStyleEl("mousse-ui-theme-color");
+const onLoade = {
+    firstLoade: true
+}
 
-        function themToString (colors) {
-            let themeStringArry = Object.keys(colors).map(key => {
-                if (Object.hasOwnProperty.call(colors, key)) {
-                    return `--${key}:${colors[key]}`;
-                }
-            });
-            return `:root{${themeStringArry.join(";")}}`;
-        }
+function use (themes, auto, darkMode) {
+    const EL = getThemeStyleEl("jet-plan-ui-theme-color");
 
-        function themeSwitch (theme) {
-            if (!auto) {
-                // 常规切换主题
-                let colors = darkMode ? theme.dark : theme.light
-                return themToString(colors);
-            } else {
-                // 自动黑暗主题
-                if (theme.dark) {
-                    const light = themToString(theme.light)
-                    const dark = themToString(theme.dark)
-                    return `
+    function themToString (colors) {
+        let themeStringArry = Object.keys(colors).map(key => {
+            if (Object.hasOwnProperty.call(colors, key)) {
+                return `--${key}:${colors[key]}`;
+            }
+        });
+        return `:root{${themeStringArry.join(";")}}`;
+    }
+
+    function themeSwitch (theme) {
+        if (!auto) {
+            // 常规切换主题
+            let colors = darkMode ? theme.dark : theme.light
+            return themToString(colors);
+        } else {
+            // 自动黑暗主题
+            if (theme.dark) {
+                const light = themToString(theme.light)
+                const dark = themToString(theme.dark)
+                return `
                     ${light}\n@media (prefers-color-scheme: dark) {${dark}}`
-                }
             }
         }
-
-        //兼容序列与单个对象
-        themes.length != undefined ?
-            setTheme(EL, themes.map(theme => {
-                return themeSwitch(theme)
-            }).join('\n')) :
-            setTheme(EL, themeSwitch(themes))
     }
+
+    //兼容序列与单个对象
+    themes.length != undefined ?
+        setTheme(EL, themes.map(theme => {
+            return themeSwitch(theme)
+        }).join('\n')) :
+        setTheme(EL, themeSwitch(themes))
+}
+
+
+export default {
+    onLoade: onLoade,
+    use: use
 }
