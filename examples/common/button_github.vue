1432559a0737a704dca66d1929de871a297da332
meme <meme_me2019@outlook.com>
2022-03-24 06:38:09 +0800

    [Bugfix] --

--- examples/common/button_github.vue
+++ examples/common/button_github.vue
@@ -1,21 +1,16 @@
 <template>
-	<j-button
-		tag="a"
-		hover
-		target="_blank"
-		href="https://github.com/abchiyi/Jet-Plan-UI"
-	>
-		<i class="bi bi-github" />
-		GitHub
-	</j-button>
+  <j-button tag="a" hover target="_blank" href="https://github.com/abchiyi/Jet-Plan-UI">
+    <i class="bi bi-github" />
+    GitHub
+  </j-button>
 </template>
 <script>
-	export default {
-		name: 'button-github',
-	};
+export default {
+  name: "button-github",
+};
 </script>
-<style>
-	.j-button {
-		background: #2d333b;
-	}
+<style scoped>
+.j-button {
+  background: #2d333b;
+}
 </style>
