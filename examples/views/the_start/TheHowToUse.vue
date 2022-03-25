9fbcd648ac92abbf44531c6d227717f2aac04d69
meme <meme_me2019@outlook.com>
2022-03-25 20:12:44 +0800

    [StyleChange] 内容可复制

--- examples/views/the_start/TheHowToUse.vue
+++ examples/views/the_start/TheHowToUse.vue
@@ -1,12 +1,13 @@
 <template>
-	<div id="the-how-to-use">
-		<h2>如何使用</h2>
-		<p>组件库支持完全引入和部分引入</p>
-		<hr />
-		<br />
-		<h3>完整引入</h3>
-		<j-code-box
-			:code="`
+  <div id="the-how-to-use">
+    <h2>如何使用</h2>
+    <p>组件库支持完全引入和部分引入</p>
+    <hr />
+    <br />
+    <h3>完整引入</h3>
+    <j-code-box
+      can-copy
+      :code="`
 
 import { resolveComponent, createApp, h } from 'vue'
 
@@ -27,14 +28,17 @@ createApp(BASE_COMPONENT)
     .use(ui)
     .mount('#app')
         `"
-		/>
-		<h3>部分引入</h3>
-		<!-- TODO  部分引入-->
-	</div>
+    >
+      <template v-slot:icon-copy>
+        <i class="bi bi-clipboard"></i> </template
+    ></j-code-box>
+    <h3>部分引入</h3>
+    <!-- TODO  部分引入-->
+  </div>
 </template>
 <script>
-	export default {
-		name: 'the-how-to-use',
-	};
+export default {
+  name: "the-how-to-use",
+};
 </script>
 <style lang=""></style>
--- examples/views/the_start/TheInstall.vue
+++ examples/views/the_start/TheInstall.vue
@@ -1,44 +1,58 @@
 <template>
-	<div id="the-insatll">
-		<h2>安装</h2>
-		<hr />
-		<br />
-		<h3>npm 安装</h3>
-		<p>使用 npm 安装以配合webpack 打包工具</p>
-		<j-code-box
-			:code="`
+  <div id="the-insatll">
+    <h2>安装</h2>
+    <hr />
+    <br />
+    <h3>npm 安装</h3>
+    <p>使用 npm 安装以配合webpack 打包工具</p>
+    <j-code-box
+      can-copy
+      :code="`
 npm i jet-plan-ui
 npm i jet-plan-ui-theme
 `"
-		></j-code-box>
-		<h3>最小文件</h3>
-		<p>从 git clone 包后自行打包文件,</p>
-		<p>打包后主文件存放在jet-plan-ui/lib目录下</p>
-		<p calss="text-hint">
-			Gitee:
-			<j-code-box
-				:code="`
+    >
+      <template v-slot:icon-copy>
+        <i class="bi bi-clipboard"></i>
+      </template>
+    </j-code-box>
+    <h3>最小文件</h3>
+    <p>从 git clone 包后自行打包文件,</p>
+    <p>打包后主文件存放在jet-plan-ui/lib目录下</p>
+    <p calss="text-hint">
+      Gitee:
+      <j-code-box
+        can-copy
+        :code="`
 https://gitee.com/abchiyi/jet-plan-ui.git
 cd ./Jet-Plan-UI
 npm run lib
 `"
-			/>
-		</p>
-		<p calss="text-hint">
-			Github :
-			<j-code-box
-				:code="`
+      >
+        <template v-slot:icon-copy>
+          <i class="bi bi-clipboard"></i>
+        </template>
+      </j-code-box>
+    </p>
+    <p calss="text-hint">
+      Github :
+      <j-code-box
+        can-copy
+        :code="`
 https://github.com/abchiyi/Jet-Plan-UI.git
 cd ./Jet-Plan-UI
 npm run lib
 `"
-			/>
-		</p>
-	</div>
+      >
+        <template v-slot:icon-copy>
+          <i class="bi bi-clipboard"></i> </template
+      ></j-code-box>
+    </p>
+  </div>
 </template>
 <script>
-	export default {
-		name: 'the-install',
-	};
+export default {
+  name: "the-install",
+};
 </script>
 <style></style>
