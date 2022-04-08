04c54669cdefb7d33fd1e9266b49c93038863925
meme <meme_me2019@outlook.com>
2022-04-08 20:32:29 +0800

    [UpdateDoc] 更新文档

--- examples/views/the_demo/TheDemoAllThemes.vue
+++ examples/views/the_demo/TheDemoAllThemes.vue
@@ -1,15 +1,17 @@
 <template>
-  <h1>&lt;j-all-theme&gt;</h1>
-  <p>
-    组件 &lt;j-all-theme&gt;
-    组件用于显示所有已安装的主题，并通过点击图标来切换主题
-  </p>
-  <demo-box title="<j-all-theme />" :code="code" expand>
-    <div>
-      <j-all-theme />
-    </div>
-  </demo-box>
-  <hr />
+  <div>
+    <h1>&lt;j-all-theme&gt;</h1>
+    <p>
+      组件 &lt;j-all-theme&gt;
+      组件用于显示所有已安装的主题，并通过点击图标来切换主题
+    </p>
+    <hr />
+    <demo-box title="<j-all-theme />" :code="code" expand>
+      <div>
+        <j-all-theme />
+      </div>
+    </demo-box>
+  </div>
 </template>
 
 <script>
