c20dbc11e56934515e887ba8302a631283c57378
meme <meme_me2019@outlook.com>
2022-04-09 01:05:33 +0800

    [FixBug] 修正在firefox浏览器中'.dot'的动画的错误

--- packages/jet-plan-ui/form/src/Radio.vue
+++ packages/jet-plan-ui/form/src/Radio.vue
@@ -1,171 +1,165 @@
 <template>
-  <div @click="click" :class="classes">
-    <svg id="radio" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-      <rect class="background" width="16" height="16" rx="8" />
-      <rect
-        class="mask"
-        width="12"
-        height="12"
-        rx="6"
-        transform="translate(2 2)"
-      />
-      <rect
-        class="dot"
-        width="6"
-        height="6"
-        rx="3"
-        transform="translate(5 5)"
-      />
-    </svg>
-  </div>
+    <div @click="click" :class="classes">
+        <svg id="radio" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <rect class="background" width="16" height="16" rx="8" />
+            <rect
+                class="mask"
+                width="12"
+                height="12"
+                rx="6"
+                transform="translate(2 2)"
+            />
+            <g class="dot">
+                <rect width="6" height="6" rx="3" transform="translate(5 5)" />
+            </g>
+        </svg>
+    </div>
 
-  <input
-    :id="id"
-    :name="name"
-    v-show="false"
-    :value="value"
-    type="radio"
-    :disabled="disabled"
-    v-model="localvalue"
-  />
+    <input
+        :id="id"
+        :name="name"
+        v-show="false"
+        :value="value"
+        type="radio"
+        :disabled="disabled"
+        v-model="localvalue"
+    />
 </template>
 <script>
 export default {
-  name: 'j-radio',
-  methods: {
-    click() {
-      if (!this.disabled) {
-        this.$emit('update:modelValue', this.value);
-      }
+    name: 'j-radio',
+    methods: {
+        click() {
+            if (!this.disabled) {
+                this.$emit('update:modelValue', this.value);
+            }
+        },
     },
-  },
-  computed: {
-    select() {
-      return this.modelValue === this.value;
+    computed: {
+        select() {
+            return this.modelValue === this.value;
+        },
+        classes() {
+            return [
+                'shape j-radio',
+                this.size,
+                this.select ? 'select' : '',
+                this.disabled ? 'disabled' : '',
+            ];
+        },
     },
-    classes() {
-      return [
-        'shape j-radio',
-        this.size,
-        this.select ? 'select' : '',
-        this.disabled ? 'disabled' : '',
-      ];
+    props: {
+        modelValue: {
+            required: true,
+        },
+        value: {
+            required: true,
+        },
+        name: String,
+        id: {
+            required: true,
+        },
+        size: {
+            type: String,
+            default: 's',
+            validator: (v) => {
+                return ['s', 'm', 'l'].indexOf(v) !== -1;
+            },
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
     },
-  },
-  props: {
-    modelValue: {
-      required: true,
+    data() {
+        return {
+            localvalue: this.modelValue,
+        };
     },
-    value: {
-      required: true,
+    watch: {
+        modelValue(d) {
+            this.localvalue = d;
+        },
+        localvalue(d) {
+            this.$emit('update:modelValue', d);
+            this.$emit('change', this.select);
+        },
     },
-    name: String,
-    id: {
-      required: true,
-    },
-    size: {
-      type: String,
-      default: 's',
-      validator: (v) => {
-        return ['s', 'm', 'l'].indexOf(v) !== -1;
-      },
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      localvalue: this.modelValue,
-    };
-  },
-  watch: {
-    modelValue(d) {
-      this.localvalue = d;
-    },
-    localvalue(d) {
-      this.$emit('update:modelValue', d);
-      this.$emit('change', this.select);
-    },
-  },
 };
 </script>
 
 <style>
 /*--------------- Animation --------------- */
 .j-radio * {
-  transition: 0.3s cubic-bezier(0.3, 0.6, 0.15, 1.3);
+    transition: 0.3s cubic-bezier(0.3, 0.6, 0.15, 1.3);
 }
 
 /*--------------- Default --------------- */
 .j-radio {
-  display: inline-block;
-  height: var(--HEIGHT);
-  width: var(--HEIGHT);
-  position: relative;
-  user-select: none;
-  margin: 0 2px;
-  cursor: pointer;
+    display: inline-block;
+    height: var(--HEIGHT);
+    width: var(--HEIGHT);
+    position: relative;
+    user-select: none;
+    margin: 0 2px;
+    cursor: pointer;
 }
 
 .j-radio .background {
-  opacity: 0.6;
-  fill: var(--border);
+    opacity: 0.6;
+    fill: var(--border);
 }
 
 .j-radio .mask {
-  fill: var(--background);
+    fill: var(--background);
 }
 
 .j-radio .dot {
-  animation: radio-dot-out 0.6s cubic-bezier(0.3, 0.6, 0.15, 1.3);
-  fill: var(--text-light);
-  opacity: 0;
+    animation: radio-dot-out 0.6s cubic-bezier(0.3, 0.6, 0.15, 1.3);
+    fill: var(--text-light);
+    opacity: 0;
 }
 @keyframes radio-dot-out {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    transform: translate(50%, 50%);
-    opacity: 0;
-    height: 0;
-    width: 0;
-  }
+    0% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+        height: 0;
+        width: 0;
+    }
 }
 /*--------------- Select --------------- */
 .j-radio.select .mask,
 .j-radio.select .background {
-  fill: var(--primary);
+    fill: var(--primary);
 }
 
 .j-radio.select .dot {
-  animation: radio-dot-in 0.3s cubic-bezier(0.3, 0.6, 0.15, 1.3);
-  fill: var(--text-light);
-  opacity: 1;
+    animation: radio-dot-in 0.3s cubic-bezier(0.3, 0.6, 0.15, 1.3);
+    fill: var(--text-light);
+    opacity: 1;
 }
 @keyframes radio-dot-in {
-  0% {
-    transform: translate(50%, 50%);
-    opacity: 0;
-    height: 0;
-    width: 0;
-  }
+    0% {
+        opacity: 0;
+        height: 0;
+        width: 0;
+    }
 }
 /*--------------- Disabled --------------- */
 .j-radio.j-radio.disabled {
-  cursor: not-allowed;
+    cursor: not-allowed;
 }
 .j-radio.disabled .mask {
-  fill: var(--disabled);
+    fill: var(--disabled);
 }
 
 .j-radio.disabled.select .background {
-  fill: var(--border);
+    fill: var(--border);
 }
 
 .j-radio.disabled.select .dot {
-  opacity: 0.5;
+    opacity: 0.5;
 }
 </style>
