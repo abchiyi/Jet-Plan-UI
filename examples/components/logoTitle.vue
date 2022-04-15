cd415dfc05c768b89f391c36b3aba3cb073c0815
meme <meme_me2019@outlook.com>
2022-04-15 22:27:48 +0800

    --

--- examples/components/logoTitle.vue
+++ examples/components/logoTitle.vue
@@ -1,25 +1,27 @@
 <template>
-	<j-row class="logo-header" X="center" Y="center">
-		<j-button class="text-hint" text @click="toHomePage">
-			<j-row X="center" Y="center">
-				<img src="../assets/logo.png" alt="logo" height="30" />
-				<span class="version">Jet UI - V0.87.1beta</span>
-			</j-row>
-		</j-button>
-	</j-row>
+    <j-row class="logo-header" X="center" Y="center">
+        <j-button class="text-hint" text @click="toHomePage">
+            <j-row X="center" Y="center">
+                <img src="../assets/logo.png" alt="logo" height="30" />
+                <span class="version"
+                    >JetPlanUI - <span class="font-mono">V0.1.9</span></span
+                >
+            </j-row>
+        </j-button>
+    </j-row>
 </template>
 <script>
-	export default {
-		name: 'logo-title',
-		methods: {
-			toHomePage() {
-				this.$router.push('/');
-			},
-		},
-	};
+export default {
+    name: 'logo-title',
+    methods: {
+        toHomePage() {
+            this.$router.push('/');
+        },
+    },
+};
 </script>
 <style scoped>
-	.logo-header {
-		height: 72px;
-	}
+.logo-header {
+    height: 72px;
+}
 </style>
