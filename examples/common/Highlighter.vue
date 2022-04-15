f814b93b5cf74279704f35f9d12fa69aeb1b6b3d
meme <meme_me2019@outlook.com>
2022-04-16 02:09:41 +0800

    [StyleChange] --

--- examples/common/Highlighter.vue
+++ examples/common/Highlighter.vue
@@ -1,27 +1,26 @@
 <template>
-  <code class="high-lighter font-mono">
-    <slot>inline code</slot>
-  </code>
+    <code class="high-lighter font-mono">
+        <slot>inline code</slot>
+    </code>
 </template>
 
 <script>
 export default {
-  name: 'high-lighter',
+    name: 'high-lighter',
 };
 </script>
 
 <style>
 .warning-text,
 code.high-lighter {
-  outline: solid 2px var(--border);
-  background-color: var(--border);
-  border-radius: var(--s-radius);
-  color: var(--text-hint);
-  display: inline-block;
-  padding: 0 6px;
+    background-color: var(--border);
+    border-radius: var(--s-radius);
+    color: var(--text-hint);
+    display: inline-block;
+    padding: 0 6px;
 }
 
 .high-lighter + .high-lighter {
-  margin-left: 5px;
+    margin-left: 5px;
 }
 </style>
