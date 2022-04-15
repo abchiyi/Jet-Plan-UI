30ec69cb1531e083b292a5ee4c396971c212a39a
meme <meme_me2019@outlook.com>
2022-04-16 00:59:51 +0800

    [DocUpdate] 修改文档结构

--- examples/views/the_demo/the_demo_tools/DemoTimedActionLimit.vue
+++ examples/views/the_demo/the_demo_tools/DemoTimedActionLimit.vue
@@ -1,98 +1,91 @@
 <template>
-  <div id="time-action-limit">
-    <p>
-      工具存放以下路径中
-      <high-lighter>tool/lib</high-lighter>
-    </p>
-    <!--TimeActionLimit -->
     <article id="time-action-limit">
-      <doc-item name="TimedActionLimit:" title-is="h2">
-        <p>限制单位时间内所能执行操作的次数。</p>
-        <doc-item name="Args:" title-is="h3">
-          <li>
-            <high-lighter>timeout:Number</high-lighter>
-            动作等待时间，单位为ms。
-            <strong style="font-size: 1em">这个值是必须的</strong>
-          </li>
-          <li>
-            <high-lighter>limt:Number</high-lighter>
-            设定过热前可执行动作次数。
-            <high-lighter>default:1</high-lighter>
-          </li>
-          <li>
-            <high-lighter>waitCoolingDown:Boolean</high-lighter>
-            是否要求完全冷却后方可执行动作。如设定为‘true’时，在冷却完成前执行动作则会重置冷却时间。
-            <high-lighter>default:true</high-lighter>
-          </li>
+        <doc-item name="TimedActionLimit:" title-is="h2" no-dot no-padding>
+            <p>限制单位时间内所能执行操作的次数。</p>
+            <hr />
+            <doc-item name="Args:" title-is="h3">
+                <li>
+                    <high-lighter>timeout:Number</high-lighter>
+                    动作等待时间，单位为ms。
+                    <strong style="font-size: 1em">这个值是必须的</strong>
+                </li>
+                <li>
+                    <high-lighter>limt:Number</high-lighter>
+                    设定过热前可执行动作次数。
+                    <high-lighter>default:1</high-lighter>
+                </li>
+                <li>
+                    <high-lighter>waitCoolingDown:Boolean</high-lighter>
+                    是否要求完全冷却后方可执行动作。如设定为‘true’时，在冷却完成前执行动作则会重置冷却时间。
+                    <high-lighter>default:true</high-lighter>
+                </li>
+            </doc-item>
+
+            <doc-item name="Methods:" title-is="h3">
+                <li>
+                    <high-lighter>.action(callback)</high-lighter>
+                    将需要执行的函数作为参数传入，即可对该函数进行单位时间执行次数限制。
+                </li>
+                <li>
+                    <high-lighter>.setCooledAlarm(callback)</high-lighter>
+                    通知外部程序已冷却完成。
+                </li>
+                <li>
+                    <high-lighter> .setOverheatAlarm(callback) </high-lighter>
+                    通知外部程序已过热。
+                </li>
+            </doc-item>
         </doc-item>
 
-        <doc-item name="methos:" title-is="h3">
-          <li>
-            <high-lighter>.action(callback)</high-lighter>
-            将需要执行的函数作为参数传入，即可对该函数进行单位时间执行次数限制。
-          </li>
-          <li>
-            <high-lighter>.setCooledAlarm(callback)</high-lighter>
-            通知外部程序已冷却完成。
-          </li>
-          <li>
-            <high-lighter> .setOverheatAlarm(callback) </high-lighter>
-            通知外部程序已过热。
-          </li>
+        <doc-item name="示例:" title-is="h2" no-padding no-dote>
+            <p>
+                在下方 Demo 中限制为
+                2s内可执行2次操作,你需要连续点击它来对比两种模式的差异
+            </p>
+            <hr />
+
+            <j-control-bar for-id="demo-wcd-control">
+                <template v-slot:text>
+                    {{ wcd ? '阻塞式等待' : '动态等待' }}
+                </template>
+                <j-switch id="demo-wcd-control" v-model="wcd" />
+            </j-control-bar>
+            <demo-box title="TimedActionLimit" :code="code">
+                <!-- Demo -->
+                <div>
+                    <!-- 冷却进度条 -->
+                    <div
+                        :class="coolDownProgressClass"
+                        id="demo-cool-down-progress"
+                        class="font-mono"
+                    >
+                        <j-row X="center">{{ text }}</j-row>
+                    </div>
+                    <!-- 点击示例 -->
+                    <j-cube
+                        id="demo-show-click-counter"
+                        class="font-mono"
+                        @click="click"
+                        style="user-select: none"
+                        :style="{ fontSize: clickCounter == 0 ? '1em' : '' }"
+                    >
+                        {{ clickCounter == 0 ? 'ClickMe!!' : clickCounter }}
+                    </j-cube>
+                </div>
+            </demo-box>
         </doc-item>
-        <template v-slot:use>
-          <li>
-            <!-- XXX 可能变更的包名  mousse-ui -->
-            导入路径:
-          </li>
-          <j-code-box
-            code="import { TimedActionLimit } from mousse-ui/tool/lib"
-          />
-        </template>
-      </doc-item>
-      <p>在下方 Demo 中限制为 2s内可执行2次操作</p>
-      <j-control-bar for-id="demo-wcd-control">
-        <template v-slot:text>
-          {{ wcd ? '阻塞式等待' : '动态等待' }}
-        </template>
-        <j-switch id="demo-wcd-control" v-model="wcd" />
-      </j-control-bar>
-      <demo-box title="TimedActionLimit" :code="code">
-        <!-- Demo -->
-        <div>
-          <!-- 冷却进度条 -->
-          <div
-            :class="cooldownProgressClass"
-            id="demo-cooldown-progress"
-            class="font-mono"
-          >
-            <j-row X="center">{{ text }}</j-row>
-          </div>
-          <!-- 点击示例 -->
-          <j-cube
-            id="demo-show-click-conter"
-            class="font-mono"
-            @click="click"
-            style="user-select: none"
-            :style="{ fontSize: clickConter == 0 ? '1em' : '' }"
-          >
-            {{ clickConter == 0 ? 'ClickMe!!' : clickConter }}
-          </j-cube>
-        </div>
-      </demo-box>
     </article>
-  </div>
 </template>
 <script>
 import { TimedActionLimit } from '@ui/tool/lib';
 // const tal = new TimedActionLimit(2000, 2);
 export default {
-  name: 'the-tool-lib',
-  data: () => {
-    return {
-      code: `//注意！code 示例为 demo 的简化版本
+    name: 'the-tool-lib',
+    data: () => {
+        return {
+            code: `//注意！code 示例为 demo 的简化版本
 //JavaScript
-let clickConter = 0;
+let clickCounter = 0;
 // 控制是否等待冷却完成
 let wcd = true;
 const tal = new TimedActionLimit(2000,2,wcl);
@@ -106,97 +99,97 @@ tal.setOverheatAlarm(() => {
 
 function click() {
     tal.action(()=>{
-        clickConter++;
+        clickCounter++;
     })
 }
 
 //HTML
 <body>
-    <p onclick="click()">{{ clickConter == 0? 'ClickMe!!':clickConter }}</p>
+    <p onclick="click()">{{ clickCounter == 0? 'ClickMe!!':clickCounter }}</p>
 </body>
       `,
-      clickConter: 0,
-      overheat: false,
-      text: 'Cooled!',
-      wcd: true,
-    };
-  },
-  mounted() {},
-  watch: {
-    overheat(v) {
-      if (v) {
-        setTimeout(() => {
-          this.overheat = false;
-          this.text = 'Cooling...';
-        }, 1000);
-      }
+            clickCounter: 0,
+            overheat: false,
+            text: 'Cooled!',
+            wcd: true,
+        };
     },
-  },
-  methods: {
-    click() {
-      this.tal.action(() => {
-        this.clickConter++;
-      });
+    mounted() {},
+    watch: {
+        overheat(v) {
+            if (v) {
+                setTimeout(() => {
+                    this.overheat = false;
+                    this.text = 'Cooling...';
+                }, 1000);
+            }
+        },
     },
-  },
-  computed: {
-    cooldownProgressClass() {
-      return [
-        this.overheat ? 'overheat' : '',
-        !this.overheat && this.clickConter > 1 ? 'cooling' : '',
-      ];
+    methods: {
+        click() {
+            this.tal.action(() => {
+                this.clickCounter++;
+            });
+        },
     },
-    tal() {
-      let tal = new TimedActionLimit(2000, 2, this.wcd);
-      tal.setCooledAlarm(() => {
-        this.text = 'Cooled!';
-      });
-      tal.setOverheatAlarm(() => {
-        this.overheat = true;
-        this.text = 'Overheat!';
-      });
-      return tal;
+    computed: {
+        coolDownProgressClass() {
+            return [
+                this.overheat ? 'overheat' : '',
+                !this.overheat && this.clickCounter > 1 ? 'cooling' : '',
+            ];
+        },
+        tal() {
+            let tal = new TimedActionLimit(2000, 2, this.wcd);
+            tal.setCooledAlarm(() => {
+                this.text = 'Cooled!';
+            });
+            tal.setOverheatAlarm(() => {
+                this.overheat = true;
+                this.text = 'Overheat!';
+            });
+            return tal;
+        },
     },
-  },
 };
 </script>
 <style>
-#demo-show-click-conter {
-  font-size: 48px;
+#demo-show-click-counter {
+    font-size: 48px;
 }
-#demo-cooldown-progress {
-  background-color: var(--success);
-  border-radius: var(--s-radius);
-  display: inline-block;
-  margin-bottom: 1em;
-  color: white;
-  padding: 5px;
-  height: 1em;
-  width: 90px;
-}
-
-#demo-cooldown-progress.overheat {
-  transition: all 0.5s var(--ease-out);
-  background-color: var(--error);
-  width: 100%;
+#demo-cool-down-progress {
+    background-color: var(--success);
+    border-radius: var(--s-radius);
+    display: inline-block;
+    margin-bottom: 1em;
+    color: white;
+    padding: 5px;
+    height: 1em;
+    width: 90px;
 }
 
-#demo-cooldown-progress.cooling {
-  transition: background 0.2s var(--ease-out);
-  animation: cooling 1s var(--ease-out);
+#demo-cool-down-progress.overheat {
+    transition: all 0.5s var(--ease-out);
+    background-color: var(--error);
+    width: 100%;
+}
+
+#demo-cool-down-progress.cooling {
+    transition: background 0.2s var(--ease-out);
+    animation: cooling 1s var(--ease-out);
 }
 
 @keyframes cooling {
-  0% {
-    background-color: var(--error);
-    width: 100%;
-  }
-  80% {
-    background-color: var(--warning);
-  }
-  100% {
-    background-color: var(--success);
-    width: 90px;
-  }
+    0% {
+        background-color: var(--error);
+        width: 100%;
+    }
+    80% {
+        background-color: var(--warning);
+    }
+    100% {
+        background-color: var(--success);
+        width: 90px;
+    }
 }
 </style>
--- examples/views/the_demo/the_demo_tools/DemoTools.vue
+++ examples/views/the_demo/the_demo_tools/DemoTools.vue
@@ -1,35 +1,34 @@
 <template>
-  <div>
-    <h2>工具</h2>
-    <p>这里是一些内置的工具函数&类，以及一些指令的函数版本。</p>
-    <hr>
-    <br>
-    <demo-tal />,
-    <hr>
-    <br>
-    <demo-scroll-to />
-    <br>
-    <hr>
-    <demo-shadow-painter />
-  </div>
+    <div>
+        <h2>工具</h2>
+        <p>这里是一些内置的工具函数&类，以及一些指令的函数版本。</p>
+        <hr />
+        <br />
+        <demo-tal />
+        <hr />
+        <br />
+        <demo-scroll-to />
+        <br />
+        <hr />
+        <demo-shadow-painter />
+    </div>
 </template>
 <script>
-import pdn from '../../../common/mix/popDemoName'
-import demoTal from './DemoTimedActionLimit.vue'
-import demoScrollTo from './DemoScrollTo.vue'
-import demoShadowPainter from './DemoShadowPrinter.vue'
+import pdn from '../../../common/mix/popDemoName';
+import demoTal from './DemoTimedActionLimit.vue';
+import demoScrollTo from './DemoScrollTo.vue';
+import demoShadowPainter from './DemoShadowPrinter.vue';
 export default {
-  mixins: [pdn],
-  name: 'the-demo-tools',
-  components: {
-    demoTal,
-    demoScrollTo,
-    demoShadowPainter
-  },
-  data: () => {
-    return {
-    }
-  }
+    mixins: [pdn],
+    name: 'the-demo-tools',
+    components: {
+        demoTal,
+        demoScrollTo,
+        demoShadowPainter,
+    },
+    data: () => {
+        return {};
+    },
 };
 </script>
 <style>
