22c93e878d03012f06b232212d85f20ef87113a2
meme <meme_me2019@outlook.com>
2022-04-29 23:11:36 +0800

    update doc

--- examples/views/the_demo/TheDemoAvater.vue
+++ examples/views/the_demo/TheDemoAvater.vue
@@ -7,7 +7,36 @@
             <template v-slot:text> 切换外形为矩形 </template>
             <j-switch id="c-switch-to-rectangle" v-model="toRectangle" />
         </j-control-bar>
-        <demo-box title="Avater" :code="``" expand>
+        <demo-box
+            title="Avater"
+            expand
+            code='
+<div class="demo-item text-hint">
+    <span>图标</span>
+    <j-avater>
+        <i class="bi bi-files"></i>
+    </j-avater>
+</div>
+<div class="demo-item text-hint">
+    <span>图片</span>
+    <j-avater>
+        <img
+            src="../../../public/pexels-creative-vix-9754.jpg"
+            alt="avater-bg"
+        />
+    </j-avater>
+</div>
+<div class="demo-item text-hint">
+    <span>文字</span>
+    <j-avater>A</j-avater>
+</div>
+<div class="demo-item text-hint">
+    <span>文字-多字符</span>
+    <j-avater>
+        <span style="padding: 0 10px"> Abc123 </span>
+    </j-avater>
+</div>'
+        >
             <j-row warp>
                 <div class="demo-item text-hint">
                     <span>图标</span>
@@ -30,7 +59,9 @@
                 </div>
                 <div class="demo-item text-hint">
                     <span>文字-多字符</span>
-                    <j-avater>Abc123</j-avater>
+                    <j-avater>
+                        <span style="padding: 0 10px"> Abc123 </span>
+                    </j-avater>
                 </div>
             </j-row>
         </demo-box>
