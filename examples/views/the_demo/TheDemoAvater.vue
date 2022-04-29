ff49655f8f2e428f32fca23535b597a2003f35b7
meme <meme_me2019@outlook.com>
2022-04-29 23:29:49 +0800

    update doc

--- examples/views/the_demo/TheDemoAvater.vue
+++ examples/views/the_demo/TheDemoAvater.vue
@@ -7,6 +7,10 @@
             <template v-slot:text> 切换外形为矩形 </template>
             <j-switch id="c-switch-to-circle" v-model="toCircle" />
         </j-control-bar>
+        <j-control-bar for-id="c-switch-border">
+            <template v-slot:text> 启用边框 </template>
+            <j-switch id="c-switch-border" v-model="border" />
+        </j-control-bar>
         <demo-box
             title="Avater"
             expand
@@ -40,13 +44,13 @@
             <j-row warp>
                 <div class="demo-item text-hint">
                     <span>图标</span>
-                    <j-avater :circle="toCircle">
+                    <j-avater :border="border" :circle="toCircle">
                         <i class="bi bi-files"></i>
                     </j-avater>
                 </div>
                 <div class="demo-item text-hint">
                     <span>图片</span>
-                    <j-avater :circle="toCircle">
+                    <j-avater :border="border" :circle="toCircle">
                         <img
                             src="../../../public/pexels-creative-vix-9754.jpg"
                             alt="avater-bg"
@@ -55,11 +59,11 @@
                 </div>
                 <div class="demo-item text-hint">
                     <span>文字</span>
-                    <j-avater :circle="toCircle">A</j-avater>
+                    <j-avater :border="border" :circle="toCircle">A</j-avater>
                 </div>
                 <div class="demo-item text-hint">
                     <span>文字-多字符</span>
-                    <j-avater :circle="toCircle">
+                    <j-avater :border="border" :circle="toCircle">
                         <span style="padding: 0 10px"> Abc123 </span>
                     </j-avater>
                 </div>
@@ -74,6 +78,7 @@ export default {
     data() {
         return {
             toCircle: true,
+            border: false,
         };
     },
 };
