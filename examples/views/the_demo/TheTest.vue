25b8eff58ca51ebbbd267d0dabed9f1b94f93523
meme <meme_me2019@outlook.com>
2022-05-09 11:42:03 +0800

    [Update] 添加功能 round & border

--- examples/views/the_demo/TheTest.vue
+++ examples/views/the_demo/TheTest.vue
@@ -1,6 +1,7 @@
 <template>
     <j-row X="center">
-        <div>
+        <div ref="test">
+            <j-button @click="testF"></j-button>
             <h1
                 @click="eventBus.push('click')"
                 @touchstart="eventBus.push('touchstart')"
@@ -14,19 +15,54 @@
                 Test Page
             </h1>
             <p>{{ eventBus }}</p>
+            <j-switch v-model="value" id="test-switch" />
+            <j-switch v-model="value1" id="test-switch1" />
+            <base-shape
+                :class="testClass"
+                class="show-box"
+                border="2"
+                :round="value1"
+            >
+            </base-shape>
         </div>
     </j-row>
 </template>
 <script>
+import { getOffset } from '@ui/tool/lib/dom';
+import { baseShape } from '../../../packages/jet-plan-ui/shape';
 export default {
     name: 'the-test-page',
+    components: {
+        baseShape,
+    },
     data() {
         return {
             eventBus: [],
+            value: false,
+            value1: false,
         };
+    },
+    methods: {
+        testF() {
+            console.log(getOffset(this.$refs.test));
+        },
+    },
+    computed: {
+        testClass() {
+            return [this.value ? 'xl' : ''];
+        },
     },
 };
 </script>
 
 <style>
+.show-box {
+    height: 60px;
+    width: 300px;
+    border-radius: var(--m-radius);
+}
+
+.xl {
+    height: 200px;
+}
 </style>
--- packages/jet-plan-ui/shape/__tests__/shape.spec.js
+++ packages/jet-plan-ui/shape/__tests__/shape.spec.js
@@ -14,4 +14,21 @@ describe("Component j-shape", () => {
     it("install", () => {
         expect(shape.install).toBeTruthy()
     })
+
+    it('Prop Border', async () => {
+        const wrapper = mount(baseShape)
+        expect(wrapper.vm.style.border).not.toBeTruthy()
+        await wrapper.setProps({
+            border: '2'
+        })
+        expect(wrapper.vm.style.border).toEqual("2px solid var(--border)")
+    })
+    it('Prop Round', async () => {
+        const wrapper = mount(baseShape)
+        await wrapper.setProps({
+            round: true
+        })
+        expect(wrapper.classes()).toContain('round')
+        expect(wrapper.vm.dataHeight).toMatch(/.+px/)
+    })
 })
