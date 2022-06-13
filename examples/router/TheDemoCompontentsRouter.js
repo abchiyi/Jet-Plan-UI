3415b1800467764019e809c02f05d207c76b1c71
abchiyi <meme_me2019@outlook.com>
2022-06-13 10:15:28 +0800

    [Update] 为 Bubble 组件文档设置 url 路径

--- examples/router/TheDemoCompontentsRouter.js
+++ examples/router/TheDemoCompontentsRouter.js
@@ -13,6 +13,12 @@ export default [
             '../views/the_demo/TheDemoAlert.vue')
     },
     {
+        name: "Bubble",
+        path: "/bubble",
+        component: () => import(
+            '../views/the_demo/TheDemoBubble.vue')
+    },
+    {
         name: "Card",
         path: "/card",
         component: () => import(
@@ -34,21 +40,21 @@ export default [
         path: '/test',
         component: () => import('../views/the_demo/TheTest.vue'),
         index: [{
-                name: 'div1',
-                to: "#div1"
-            },
-            {
-                name: 'div2',
-                to: "#div2"
-            },
-            {
-                name: 'div3',
-                to: "#div3"
-            },
-            {
-                name: 'div4',
-                to: "#div4"
-            },
+            name: 'div1',
+            to: "#div1"
+        },
+        {
+            name: 'div2',
+            to: "#div2"
+        },
+        {
+            name: 'div3',
+            to: "#div3"
+        },
+        {
+            name: 'div4',
+            to: "#div4"
+        },
         ]
     },
     {
@@ -60,20 +66,20 @@ export default [
         name: 'Inputs',
         path: '/inputs',
         index: [{
-                name: 'Input',
-                to: "#input"
-            },
-            {
-                name: 'Checkbox',
-                to: "#checkbox"
-            },
-            {
-                name: 'Radio',
-                to: "#radio"
-            }, {
-                name: 'Switch',
-                to: "#switch"
-            }
+            name: 'Input',
+            to: "#input"
+        },
+        {
+            name: 'Checkbox',
+            to: "#checkbox"
+        },
+        {
+            name: 'Radio',
+            to: "#radio"
+        }, {
+            name: 'Switch',
+            to: "#switch"
+        }
         ],
         component: () => import('../views/the_demo/the_demo_inputs/TheDemoInputs.vue')
     }, {
