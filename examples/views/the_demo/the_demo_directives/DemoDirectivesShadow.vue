667c47dc4eb56a575c08cef166592cfb7e8bdc57
abchiyi <meme_me2019@outlook.com>
2022-06-23 19:50:07 +0800

    [Update] 使用滑动选择器

--- examples/views/the_demo/the_demo_directives/DemoDirectivesShadow.vue
+++ examples/views/the_demo/the_demo_directives/DemoDirectivesShadow.vue
@@ -1,41 +1,39 @@
 <template>
-  <article id="shadow">
-    <doc-item name="v-shadow:" title-is="h2">
-      <li>
-        <p>
-          <high-lighter>:arg</high-lighter>
-          : range - top | nottom | left | right
-        </p>
-      </li>
-      <li>
-        <p>
-          <high-lighter>="value"</high-lighter>
-          : type - Number, range - 1~30
-        </p>
-        设置阴影强度,建议1~30,最大值可超过30
-      </li>
-    </doc-item>
-    <j-card>
-      <j-row spaceMode="between" Y="center">
-        <label for="shadow-intensity"><span>阴影强度</span></label>
-        <input id="shadow-intensity" type="number" v-model="value" />
-      </j-row>
-    </j-card>
-    <demo-box title="v-shaow" :code="code">
-      <j-row X="center">
-        <j-cube class="demo-box" v-shadow:bottom="value">
-          Intensity:{{ value }}
-        </j-cube>
-      </j-row>
-    </demo-box>
-  </article>
+    <article id="shadow">
+        <doc-item name="v-shadow:" title-is="h2">
+            <li>
+                <p>
+                    <high-lighter>:arg</high-lighter>
+                    : range - top | nottom | left | right
+                </p>
+            </li>
+            <li>
+                <p>
+                    <high-lighter>="value"</high-lighter>
+                    : type - Number, range - 1~30
+                </p>
+                设置阴影强度,建议1~30,最大值可超过30
+            </li>
+        </doc-item>
+        <j-control-bar for-id="demo-checkbox" :reverse="toReverse">
+            <template v-slot:text>Checkbox</template>
+            <j-slider max="30" v-model="value" />
+        </j-control-bar>
+        <demo-box title="v-shaow" :code="code">
+            <j-row X="center">
+                <j-cube class="demo-box" v-shadow:bottom="value">
+                    Intensity:{{ value }}
+                </j-cube>
+            </j-row>
+        </demo-box>
+    </article>
 </template>
 <script>
 export default {
-  name: 'v-shaow',
-  data: () => {
-    return {
-      code: `
+    name: 'v-shaow',
+    data: () => {
+        return {
+            code: `
 // Javascript
 let value = 10;
 // HTML
@@ -53,17 +51,17 @@ let value = 10;
         Intensity:{{value}}
     </j-cube>
 </body>`,
-      value: '10',
-    };
-  },
+            value: '10',
+        };
+    },
 };
 </script>
 <style>
 .demo-box {
-  background: var(--foreground) !important;
-  color: var(--text) !important;
-  height: 200px;
-  width: 150px;
-  border: unset;
+    background: var(--foreground) !important;
+    color: var(--text) !important;
+    height: 200px;
+    width: 150px;
+    border: unset;
 }
 </style>
