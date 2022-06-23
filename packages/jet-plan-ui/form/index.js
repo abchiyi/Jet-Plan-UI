f9b2e4c8f871b88445c5310485dbb6ba0ca190e2
abchiyi <meme_me2019@outlook.com>
2022-06-23 17:59:56 +0800

    [Update]全局注册 Slider 组件

--- packages/jet-plan-ui/form/index.js
+++ packages/jet-plan-ui/form/index.js
@@ -3,21 +3,24 @@ import Button from './src/Button.vue'
 import Switch from './src/Switch.vue'
 import Input from './src/Input.vue'
 import Radio from './src/Radio.vue'
+import Slider from './Slider.vue'
 
 export {
     CheckBox,
     Button,
     Switch,
     Radio,
-    Input
+    Input,
+    Slider
 }
 
 export default {
-    install(Vue) {
+    install (Vue) {
         Vue.component(CheckBox.name, CheckBox)
         Vue.component(Button.name, Button)
         Vue.component(Switch.name, Switch)
         Vue.component(Radio.name, Radio)
         Vue.component(Input.name, Input)
+        Vue.component(Slider.name, Slider)
     }
 }
