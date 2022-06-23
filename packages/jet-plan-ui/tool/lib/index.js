826a92438c21ff033034ce6d29a2d7d64684e62c
abchiyi <meme_me2019@outlook.com>
2022-06-23 16:48:11 +0800

    [Update] 添加重置计数器方法

--- packages/jet-plan-ui/tool/lib/index.js
+++ packages/jet-plan-ui/tool/lib/index.js
@@ -1,5 +1,5 @@
 export class TimedActionLimit {
-    constructor(timeout, limit = 1, waitCoolingDown = true) {
+    constructor (timeout, limit = 1, waitCoolingDown = true) {
         if (!timeout) {
             throw SyntaxError(
                 `
@@ -7,81 +7,84 @@ Class TimedActionLimit([timeout,[limit,waitCoolingDown]])
 Missing Value "timeout"
 Missing values ‘timeout’ are required
 `
-            );
+            )
         }
-        this.waitCoolingDown = waitCoolingDown;
-        this.limit = limit;
-        this.timeout = timeout;
-        this.timeoutID = [];
-        this.overheat = false;
-        this.conter = limit;
-        this.cooledAlarm = false;
-        this.overheatAlarm = false;
+        this.waitCoolingDown = waitCoolingDown
+        this.limit = limit
+        this.timeout = timeout
+        this.timeoutID = []
+        this.overheat = false
+        this.conter = limit
+        this.cooledAlarm = false
+        this.overheatAlarm = false
     }
 
-    action(callback) {
+    action (callback) {
         if (!this.overheat) {
-            this.conter = this.conter - 1;
-            this.overheat = this.conter == 0;
-            callback();
+            this.conter = this.conter - 1
+            this.overheat = this.conter == 0
+            callback()
         }
         if (this.overheat) {
             // 通知外部程序已过热
-            if (this.overheatAlarm) this.overheatAlarm();
+            if (this.overheatAlarm) this.overheatAlarm()
             // 设置冷却动作
-            this.__setCooling();
+            this.__setCooling()
             // 清除冷却动作
-            this.__waitControl();
+            this.__waitControl()
         }
     }
 
-    __setCooling() {
+    __setCooling () {
         // 设置冷却动作
         this.timeoutID.push(
             setTimeout(() => {
-                this.overheat = false;
+                this.overheat = false
                 // 通知外部程序已冷却
-                if (this.cooledAlarm) this.cooledAlarm();
-                this.timeoutID = [];
+                if (this.cooledAlarm) this.cooledAlarm()
+                this.timeoutID = []
             }, this.timeout)
-        );
-        this.conter = this.limit;
+        )
+        this.conter = this.limit
     }
 
-    __waitControl() {
+    __waitControl () {
         if (this.timeoutID.length > 1) {
             const clId = this.waitCoolingDown ?
                 this.timeoutID.shift() :
-                this.timeoutID.pop();
-            clearTimeout(clId);
+                this.timeoutID.pop()
+            clearTimeout(clId)
         }
     }
 
-    setCooledAlarm(callback) {
-        this.cooledAlarm = callback;
+    setCooledAlarm (callback) {
+        this.cooledAlarm = callback
     }
 
-    setOverheatAlarm(callback) {
-        this.overheatAlarm = callback;
+    setOverheatAlarm (callback) {
+        this.overheatAlarm = callback
+    }
+    reSetConter () {
+        this.conter = 0
     }
 }
 
-export function installComponent(Vue, array) {
+export function installComponent (Vue, array) {
     array.forEach((component) => {
-        Vue.component(component.name, component);
-    });
+        Vue.component(component.name, component)
+    })
 }
-export function installDirective(Vue, array) {
+export function installDirective (Vue, array) {
     array.forEach((directive) => {
-        Vue.directive(directive.name, directive);
-    });
+        Vue.directive(directive.name, directive)
+    })
 }
 
-export function hexToRgb(hex) {
-    function parse(v) {
-        return parseInt(`0x${v}`);
+export function hexToRgb (hex) {
+    function parse (v) {
+        return parseInt(`0x${v}`)
     }
-    if (!hex[0] == "#") hex = '#' + hex;
+    if (!hex[0] == "#") hex = '#' + hex
 
 
     let rgb = {
@@ -89,7 +92,7 @@ export function hexToRgb(hex) {
         g: parse(hex.slice(3, 5)),
         b: parse(hex.slice(5, 7)),
         opacity: parseFloat(parse(hex.slice(7)) / 255)
-    };
+    }
     const max = Math.abs(
         parseFloat(
             (parseInt(rgb.r) + parseInt(rgb.g) + parseInt(rgb.b)) /
@@ -101,26 +104,26 @@ export function hexToRgb(hex) {
             `rgba(${rgb.r},${rgb.g},${rgb.b},${rgb.opacity})` : `rgb(${rgb.r},${rgb.g},${rgb.b})`,
         opacity: isNaN(rgb.opacity) ? 1 : rgb.opacity,
         max: rgb.opacity ? max * rgb.opacity : max,
-    };
+    }
 }
 
-export function rgbToHex(rgb) {
-    function colorToHex(color) {
-        var hexadecimal = color.toString(16);
-        return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+export function rgbToHex (rgb) {
+    function colorToHex (color) {
+        var hexadecimal = color.toString(16)
+        return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal
     }
-    const leftBrackets = rgb.indexOf("(") + 1;
-    const rightBrackets = rgb.indexOf(")");
-    let rgba = rgb.substring(leftBrackets, rightBrackets).split(",");
-    let s = ["#"];
-    let conter = 0;
+    const leftBrackets = rgb.indexOf("(") + 1
+    const rightBrackets = rgb.indexOf(")")
+    let rgba = rgb.substring(leftBrackets, rightBrackets).split(",")
+    let s = ["#"]
+    let conter = 0
     rgba.forEach((v) => {
-        conter++;
-        v = v.replace(/^\s+|\s+$/gm, "");
+        conter++
+        v = v.replace(/^\s+|\s+$/gm, "")
         s.push(
             conter < 4 ? colorToHex(Math.abs(v)) : colorToHex(parseInt(v * 255))
-        );
-    });
+        )
+    })
 
     const max = Math.abs(
         parseFloat(
@@ -133,13 +136,13 @@ export function rgbToHex(rgb) {
         color: s.join(""),
         opacity: parseFloat(rgba[3] ? rgba[3] : 1),
         max: rgba[3] ? max * rgba[3] : max
-    };
+    }
 }
 
 // TODO 需要添加文档
-export function autoTextColor(el) {
-    function getBgColor(el) {
-        return window.getComputedStyle(el).backgroundColor;
+export function autoTextColor (el) {
+    function getBgColor (el) {
+        return window.getComputedStyle(el).backgroundColor
     }
     let hexColor = rgbToHex(getBgColor(el))
     if (hexColor.opacity == 0) {
@@ -153,7 +156,7 @@ export function autoTextColor(el) {
     }
 }
 
-export function* toBodyNode(el) {
+export function* toBodyNode (el) {
     // 从任意node节点迭代至body节点
     let stop = false
     let node = el
@@ -165,15 +168,15 @@ export function* toBodyNode(el) {
 }
 
 
-export function searchBackgroundColor(el) {
-    function getBgColor(el) {
+export function searchBackgroundColor (el) {
+    function getBgColor (el) {
         return rgbToHex(
             window.getComputedStyle(el).backgroundColor
         )
     }
 
     let color = getBgColor(el)
-    if (color.opacity) return color;
+    if (color.opacity) return color
 
     let node = toBodyNode(el)
     let stop = false
@@ -181,7 +184,7 @@ export function searchBackgroundColor(el) {
         let g = node.next()
         if (g.value) {
             let color = getBgColor(g.value)
-            if (color.opacity) return color;
+            if (color.opacity) return color
         } else {
             stop = true
         }
@@ -189,29 +192,29 @@ export function searchBackgroundColor(el) {
     return '#00000000'
 }
 
-export function reTextColor(el) {
-    function getBgColor(el) {
-        return window.getComputedStyle(el).backgroundColor;
+export function reTextColor (el) {
+    function getBgColor (el) {
+        return window.getComputedStyle(el).backgroundColor
     }
     let hexColor = rgbToHex(getBgColor(el))
     if (hexColor.opacity == 0) {
         el.style.color = ''
     } else {
         if (hexColor.max < 0.6) {
-            el.style.color = "var(--text-light)";
+            el.style.color = "var(--text-light)"
         } else {
-            el.style.color = "var(--text-dark)";
+            el.style.color = "var(--text-dark)"
         }
     }
 }
 
-export function propInit(_type, _default) {
+export function propInit (_type, _default) {
     return {
         type: _type,
         default: _default,
-    };
+    }
 }
 
-export function propInitBoolean(default_value) {
+export function propInitBoolean (default_value) {
     return propInit(Boolean, default_value)
 }
