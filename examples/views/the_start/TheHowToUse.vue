8d46168c999e5080d845200653d1498cdf504344
abchiyi <meme_me2019@outlook.com>
2022-06-28 13:24:27 +0800

    [DocUpdate]

--- examples/views/the_start/TheHowToUse.vue
+++ examples/views/the_start/TheHowToUse.vue
@@ -1,13 +1,12 @@
 <template>
-  <div id="the-how-to-use">
-    <h2>如何使用</h2>
-    <p>组件库支持完全引入和部分引入</p>
-    <hr />
-    <br />
-    <h3>完整引入</h3>
-    <j-code-box
-      can-copy
-      :code="`
+    <div id="the-how-to-use">
+        <h1>如何使用</h1>
+        <p>组件库支持完全引入和部分引入</p>
+        <hr />
+        <h2>完整引入</h2>
+        <j-code-box
+            can-copy
+            :code="`
 
 import { resolveComponent, createApp, h } from 'vue'
 
@@ -26,16 +25,17 @@ createApp(BASE_COMPONENT)
     .use(ui)
     .mount('#app')
         `"
-    >
-      <template v-slot:icon-copy> <i class="bi bi-clipboard"></i> </template
-    ></j-code-box>
-    <h3>部分引入</h3>
-    <!-- TODO  部分引入-->
-  </div>
+        >
+            <template v-slot:icon-copy>
+                <i class="bi bi-clipboard"></i> </template
+        ></j-code-box>
+        <h3>部分引入</h3>
+        <!-- TODO  部分引入-->
+    </div>
 </template>
 <script>
 export default {
-  name: 'the-how-to-use',
+    name: 'the-how-to-use',
 };
 </script>
 <style lang=""></style>
