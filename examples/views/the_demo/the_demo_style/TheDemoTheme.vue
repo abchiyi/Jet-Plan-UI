cbaea811a48896ff266343872685f2d7c904cc64
abchiyi <meme_me2019@outlook.com>
2022-07-11 18:00:20 +0800

    [DocUpdate]

--- examples/views/the_demo/the_demo_style/TheDemoTheme.vue
+++ examples/views/the_demo/the_demo_style/TheDemoTheme.vue
@@ -8,29 +8,28 @@
         </p>
         <hr />
 
-        <h2>主题切换器：</h2>
-        <demo-box code="<j-all-theme />" expand title="theme-switcher">
-            <j-all-theme @setTheme="onSetTheme" />
-        </demo-box>
+        <article id="theme-switcher">
+            <h2>主题切换器：</h2>
+            <demo-box code="<j-all-theme />" expand title="theme-switcher">
+                <j-all-theme @setTheme="onSetTheme" />
+            </demo-box>
+        </article>
 
         <!-- 安装&使用主题 -->
         <doc-item
             id="theme-install-use"
-            name="使用主题"
+            name="使用主题:"
             tag="h2"
             noPadding
             noDot
         >
-            <template v-slot:title>
-                <hr />
-            </template>
-            <li id="set-theme-at-main-file">
-                <p class="text-hint">在'main.js'（app 入口 js）:</p>
-                <j-code-box
-                    can-copy
-                    :code="`
-// main.js
-
+            <hr />
+            <ul>
+                <li id="set-theme-at-main-file">
+                    <p class="text-hint">在'main.js'（app 入口 js）:</p>
+                    <demo-box
+                        title="main.js"
+                        code="
 // 导入ui和主题
 import ui from 'jet-plan-ui'
 import { jetPlanLight } from 'jet-plan-ui-theme'
@@ -43,17 +42,18 @@ ui.$jetTheme.installTheme(
 
 // 通过向 '.setTheme' 方法指定 主题的安装名来应用主题
 ui.$jetTheme.setTheme('jet-ui-light')
-          `"
-                />
-            </li>
-            <li id="set-theme-at-component">
-                <p>在组件中更改/安装主题:</p>
-                <p class="text-hint">
-                    $JetTheme 是全局注册的你可以在任何地方调用它。比如在组件中
-                </p>
-                <j-code-box
-                    can-copy
-                    :code="`
+                    "
+                    />
+                </li>
+                <li id="set-theme-at-component">
+                    <p>在组件中更改/安装主题:</p>
+                    <p class="text-hint">
+                        $JetTheme
+                        是全局注册的你可以在任何地方调用它。比如在组件中
+                    </p>
+                    <demo-box
+                        title="NewVueComponent"
+                        code="
 import { jetPlanDark } from 'jet-plan-ui-theme'
 
 export default{
@@ -68,44 +68,42 @@ export default{
         this.$jetTheme.setTheme('jet-ui-dark')
     }
 }
-          `"
-                />
-            </li>
-            <li id="use-theme-pack">
-                <p>使用主题包:</p>
-                <p class="text-hint">
-                    主题控制的 '.install'
-                    方法接受一个对象或一个函数以一次性安装主题包内有主题
-                </p>
-                <j-code-box
-                    can-copy
-                    :code="`
-import JetPlanUI from 'jet-plan-ui';
+                    "
+                    />
+                </li>
+                <li id="use-theme-pack">
+                    <p>使用主题包:</p>
+                    <p class="text-hint">
+                        主题控制的 '.install'
+                        方法接受一个对象或一个函数以一次性安装主题包内有主题
+                    </p>
+                    <demo-box
+                        title=""
+                        code="
 import jetPlanTheme from 'jet-plan-ui-theme';
+import JetPlanUI from 'jet-plan-ui';
 
 ui.$jetTheme.install(
     JetPlanTheme
-);
-          `"
-                />
-            </li>
+);"
+                    />
+                </li>
+            </ul>
         </doc-item>
         <!-- 创建主题 -->
-        <doc-item id="theme-create" name="创建主题" tag="h2" noPadding>
-            <template v-slot:title>
-                <p>在这里讲述了如何快速定制属于你的主题</p>
-                <hr />
-            </template>
-            <li id="theme-create-extend">
-                <h3>从现有主题创建新主题</h3>
-                <p>
-                    使用 '$jetTheme.installTheme' 方法来从现有主题拓展一个新主题
-                </p>
-                <j-code-box
-                    can-copy
-                    :code="`
-// main.js
-
+        <doc-item id="theme-create" name="创建主题" tag="h2" no-dot noPadding>
+            <p>在这里讲述了如何快速定制属于你的主题</p>
+            <hr />
+            <ul>
+                <li id="theme-create-extend">
+                    <h3>从现有主题创建新主题</h3>
+                    <p>
+                        使用 '$jetTheme.installTheme'
+                        方法来从现有主题拓展一个新主题
+                    </p>
+                    <demo-box
+                        title="js"
+                        code="
 // 导入组件库和主题
 import ui from 'jet-plan-ui'
 import { jetPLanLight } from 'jet-plan-ui-theme'
@@ -118,20 +116,18 @@ ui.$jetTheme.installTheme(
     },
     jetPlanLight // 蓝本主题,其中所有的 option 将被继承,
 )
-            `"
-                />
-            </li>
-            <li id="theme-create-new">
-                <h3>创建新主题</h3>
-                <p>
-                    如需要完全重定义主题可参照以下配置。一个完整的主题对象所包含的所有
-                    'option' 参考。
-                </p>
-                <j-code-box
-                    can-copy
-                    lang="javascript"
-                    :code="`
-// jetPLanDark
+                    "
+                    />
+                </li>
+                <li id="theme-create-new">
+                    <h3>创建新主题</h3>
+                    <p>
+                        如需要完全重定义主题可参照以下配置。一个完整的主题对象所包含的所有
+                        'option' 参考。
+                    </p>
+                    <demo-box
+                        title="jetPLanDark"
+                        :code="`
  export default {
     // BACKGROUND
     foreground: '#363636',
@@ -161,55 +157,50 @@ ui.$jetTheme.installTheme(
     'text-dark': '#505050',
 }
           `"
-                />
-            </li>
-            <li id="theme-create-add-option">
-                <h3>自定义 option</h3>
-                <p>
-                    如果你的组件有额外的 'option'
-                    需求则只需在注册主题时通过拓展来加入选项，或直接在主题对象中加入
-                    'option'。要使用 'option 中定义的值时则只需通过 css
-                    变量的形式引用即可。或者从当前定义的主题 '$jetTheme.theme'
-                    中读取值
-                </p>
-                <hr />
-                <p>设置自定义 'option'</p>
-                <j-code-box
-                    can-copy
-                    :code="`
-// main.js
-
+                    />
+                </li>
+                <li id="theme-create-add-option">
+                    <h3>自定义 option</h3>
+                    <p>
+                        如果你的组件有额外的 'option'
+                        需求则只需在注册主题时通过拓展来加入选项，或直接在主题对象中加入
+                        'option'。要使用 'option 中定义的值时则只需通过 css
+                        变量的形式引用即可。或者从当前定义的主题
+                        '$jetTheme.theme' 中读取值
+                    </p>
+                    <hr />
+                    <p>设置自定义 'option'</p>
+                    <demo-box
+                        title="js"
+                        :code="`
 // 导入组件库和主题
 import ui from 'jet-plan-ui'
 import { jetPLanLight } from 'jet-plan-ui-theme'
 
 // 拓展&安装主题
 ui.$jetTheme.installTheme(
-    'jetPlanLight_Red', // 主题名称
+    'jetPlanLight_Red', // 新主题名称
     {
-        myOption:'#ef5961' // 自定义 option
+        myOption:'#ef5961' // 要覆盖/添加的 option
     },
     JetPlanLight // 蓝本主题,其中所有的 option 将被继承,
 )
 
           `"
-                />
-                <p>使用自定义 'option'</p>
-                <j-code-box
-                    can-copy
-                    lang="css"
-                    :code="`
- /* 在css中访问 */
+                    />
+                    <p>使用自定义 'option'</p>
+                    <demo-box
+                        title="css"
+                        :code="`
 .body{
-    background: var(--myOption);
+    background: var(--myOption);// 以 css 变量的形式访问自定义的 option
 }
           `"
-                />
-                <br />
-                <j-code-box
-                    can-copy
-                    lang="JavaScript"
-                    :code="`
+                    />
+                    <p class="text-hint">访问当前已应用的主题</p>
+                    <demo-box
+                        title="js"
+                        :code="`
  // 在组件中调用当前主题
 {
     mounted(){
@@ -218,26 +209,21 @@ ui.$jetTheme.installTheme(
     }
 }
           `"
-                />
-            </li>
+                    />
+                </li>
+            </ul>
         </doc-item>
+
         <doc-item
             id="theme-auto-dark-mode"
-            name="创建自动深色模式"
+            name="创建跟随操作系统主题变换明/暗的主题"
             tag="h2"
             noPadding
         >
-            <template v-slot:title>
-                <p>
-                    使用 '$jetTheme.toAuto' 方法来创建一个随系统切换的深色模式
-                </p>
-            </template>
-            <li>
-                <j-code-box
-                    can-copy
-                    :code="`
-// main.js
-
+            <p>使用 '$jetTheme.toAuto' 方法来创建一个随系统切换的深色模式</p>
+            <demo-box
+                title="js"
+                :code="`
 // 导入组件库和主题
 import ui from 'jet-plan-ui'
 import { jetPLanLight, jetPLanDark } from 'jet-plan-ui-theme'
@@ -253,8 +239,7 @@ ui.$jetTheme.installTheme(
     )
 )
             `"
-                />
-            </li>
+            />
         </doc-item>
     </div>
 </template>
