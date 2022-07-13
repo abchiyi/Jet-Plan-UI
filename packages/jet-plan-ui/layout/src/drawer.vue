e905cd2474cd9327daec085cb45f981831c8cbe6
abchiyi <meme_me2019@outlook.com>
2022-07-13 17:43:00 +0800

    [Change] props 使用初始化函数

--- packages/jet-plan-ui/layout/src/drawer.vue
+++ packages/jet-plan-ui/layout/src/drawer.vue
@@ -1,80 +1,79 @@
 <template>
-  <j-transition-slide :opacity="false" :position="from">
-    <div v-show="expand" :class="classes">
-      <slot />
-    </div>
-  </j-transition-slide>
+    <j-transition-slide :opacity="false" :position="from">
+        <div v-show="expand" :class="classes">
+            <slot />
+        </div>
+    </j-transition-slide>
 </template>
 
 
 <script>
+import { propInit, propInitBoolean, validatorRange } from '../../tool/lib';
 export default {
-  name: 'j-drawer',
-  props: {
-    expand: {
-      type: Boolean,
-      required: true,
+    name: 'j-drawer',
+    props: {
+        expand: propInitBoolean(true),
+        from: propInit(
+            String,
+            'right',
+            validatorRange(['top', 'left', 'right', 'bottom'])
+        ),
     },
-    from: {
-      type: String,
-      required: true,
+    data() {
+        return {
+            width: undefined,
+            show: false,
+        };
     },
-  },
-  data() {
-    return {
-      width: undefined,
-      show: false,
-    };
-  },
 
-  computed: {
-    classes() {
-      return ['j-drawers', this.from];
+    computed: {
+        classes() {
+            return ['j-drawers', this.from];
+        },
     },
-  },
 };
 </script>
 
 <style>
 .j-drawers {
-  overflow-y: scroll;
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  top: 0;
+    overflow-y: scroll;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    top: 0;
 }
 
 .j-drawers.right {
-  left: unset;
+    left: unset;
 }
 
 .j-drawers.left {
-  right: unset;
+    right: unset;
 }
 .j-drawers.top {
-  bottom: unset;
+    bottom: unset;
 }
 
 .j-drawers.bottom {
-  top: unset;
+    top: unset;
 }
 
 /* Firefox  */
 @supports (scrollbar-width: none) {
-  .j-drawers {
-    /* overflow: -moz-scrollbars-none; */
-    scrollbar-width: none;
-  }
+    .j-drawers {
+        /* overflow: -moz-scrollbars-none; */
+        scrollbar-width: none;
+    }
 }
 
 /* chrome and Safari */
 .j-drawers::-webkit-scrollbar {
-  display: none;
+    display: none;
 }
 
 /* IE 10+ */
 .j-drawers {
-  -ms-overflow-style: none;
+    -ms-overflow-style: none;
 }
 </style>
