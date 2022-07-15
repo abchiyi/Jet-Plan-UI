635e7f9123eada73c007e71f4e23c4692f6f3169
abchiyi <meme_me2019@outlook.com>
2022-07-15 16:26:49 +0800

    [DocUpdate]

--- examples/views/the_demo/TheDemoProgress.vue
+++ examples/views/the_demo/TheDemoProgress.vue
@@ -1,60 +1,12 @@
 <template>
     <div id="the-demo-progressbar">
         <!-- header -->
-        <doc-item
-            id="describes"
-            name="<j-progress>:"
-            tag="h1"
-            no-dot
-            no-padding
-        >
-            <li><p>用于展示加载进度</p></li>
-        </doc-item>
-        <hr />
-
-        <!-- Api -->
-        <doc-item name="Props:" tag="h2">
-            <li id="prop-value">
-                <p>
-                    <high-lighter>value</high-lighter>
-                    :type - String|Number, default - 0, range - 0 ~ 100
-                </p>
-                <p>控制进度条百分比</p>
-            </li>
-            <li id="prop-failed">
-                <p>
-                    <high-lighter>failed</high-lighter>
-                    : type - Boolean, default - false
-                </p>
-                <p>切换进度条为失败状态</p>
-            </li>
-            <li id="prop-height">
-                <p>
-                    <high-lighter>height</high-lighter>
-                    : type - String, default - "8px"
-                </p>
-                <p>控制状态栏行高</p>
-            </li>
-            <li>
-                <p>
-                    <high-lighter>pause</high-lighter>: : type - Boolean,
-                    default - failed
-                </p>
-                <p>切换样式到暂停状态</p>
-            </li>
-            <li>
-                <p>
-                    <high-lighter>ring</high-lighter>: type - Boolean, default -
-                    false
-                </p>
-                <p>切换样式到环形进度条</p>
-            </li>
+        <doc-item id="describes" name="Progress:" tag="h1" no-dot no-padding>
+            <p>用于展示加载进度</p>
+            <hr />
         </doc-item>
 
-        <h2>示例:</h2>
-        <hr />
-        <!-- control -->
-        <div>
+        <doc-item name="示例:" tag="h2">
             <j-control-bar for-id="demo-progress-to-succuss">
                 <template v-slot:text>完成进度条</template>
                 <j-switch id="demo-progress-to-succuss" v-model="success" />
@@ -67,114 +19,126 @@
                 <template v-slot:text> 暂停 </template>
                 <j-switch id="demo-progress-to-pause" v-model="pause" />
             </j-control-bar>
-        </div>
+            <demo-box title="Progress">
+                <div id="demo-content">
+                    <p>常规:</p>
+                    <j-progress
+                        :failed="failed"
+                        :value="number"
+                        :pause="pause"
+                    />
+                    <p>环形:</p>
+                    <p>
+                        Font size - 16px
+                        <j-progress
+                            :failed="failed"
+                            :value="number"
+                            :pause="pause"
+                            ring
+                        />
+                    </p>
+                    <p style="font-size: 24px">
+                        Font size 24px
+                        <j-progress
+                            :failed="failed"
+                            :value="number"
+                            :pause="pause"
+                            ring
+                        />
+                    </p>
+                    <p style="font-size: 32px">
+                        Font size 32px
+                        <j-progress
+                            :failed="failed"
+                            :value="number"
+                            :pause="pause"
+                            ring
+                        />
+                    </p>
+                    <p>按钮内部的：</p>
+                    <j-button
+                        @click="success = !success"
+                        style="background: var(--foreground)"
+                    >
+                        <j-progress
+                            style="font-size: 20px"
+                            :failed="failed"
+                            :value="number"
+                            :pause="pause"
+                            ring
+                        />
+                        <span style="margin-left: 10px"> {{ +number }}% </span>
+                    </j-button>
+                </div>
+            </demo-box>
+            <demo-box
+                title="Js"
+                code="
+export default {
+    data(){
+        return {
+            number: 50;
+            filed: false;
+            pause: false;
+        }
+    }
+}
+"
+            />
+            <demo-box
+                title="Html"
+                code='
+<!-- 条形进度条 -->
+<j-progress :failed="failed" :value="number" :pause="pause"/>
 
-        <!-- demo -->
-        <demo-box title="<j-progress>" :code="code" :expand="true">
-            <div id="demo-content">
-                <p>常规:</p>
-                <j-progress :failed="failed" :value="number" :pause="pause" />
-                <p>环形:</p>
-                <p>
-                    Font size - 16px
-                    <j-progress
-                        :failed="failed"
-                        :value="number"
-                        :pause="pause"
-                        ring
-                    />
-                </p>
-                <p style="font-size: 24px">
-                    Font size 24px
-                    <j-progress
-                        :failed="failed"
-                        :value="number"
-                        :pause="pause"
-                        ring
-                    />
-                </p>
-                <p style="font-size: 32px">
-                    Font size 32px
-                    <j-progress
-                        :failed="failed"
-                        :value="number"
-                        :pause="pause"
-                        ring
-                    />
-                </p>
-                <p>环形 in Button:</p>
-                <j-button
-                    @click="success = !success"
-                    style="background: var(--foreground)"
-                >
-                    <j-progress
-                        style="font-size: 20px"
-                        :failed="failed"
-                        :value="number"
-                        :pause="pause"
-                        ring
-                    />
-                    <span style="margin-left: 10px"> {{ +number }}% </span>
-                </j-button>
-            </div>
-        </demo-box>
+<!-- 环形进度条 -->
+<j-progress
+    :failed="failed"
+    :value="number"
+    :pause="pause"
+    ring
+/>
+
+<!--  在按钮内部的 -->
+<j-button>
+    <j-progress
+        ring
+        :failed="failed"
+        :value="number"
+        :pause="pause"
+    />
+    {{ number }} %
+</j-button>
+'
+            />
+        </doc-item>
+        <component-documentation v-bind="componentDoc" />
     </div>
 </template>
 
 <script>
+import { Progress } from '@ui';
 import pdn from '../../common/mix/popDemoName';
 export default {
     mixins: [pdn],
     name: 'the-demo-progress',
     data() {
         return {
+            componentDoc: {
+                component: Progress,
+                description: {
+                    props: {
+                        value: '说明：进度条百分比。',
+                        failed: '说明：更改样式状态为‘失败/错误’。',
+                        pause: '说明：更改样式状态为‘暂停’。',
+                        height: '说明：修改条形进度条高度。',
+                        ring: '说明：显示为环形进度条。',
+                    },
+                },
+            },
             code: `
-//javascript
-let number = 50;
-let filed = false;
-let pause = false;
 
-// HTML
-<j-control-bar for-id="demo-progress-to-succuss">
-    <template v-slot:text>完成进度条</template>
-        <j-switch
-            id="demo-progress-to-succuss"
-            v-model="success"
-        />
-</j-control-bar>
-<j-control-bar for-id="demo-progress-to-failed">
-    <template v-slot:text>失败进度条</template>
-        <j-switch
-            id="demo-progress-to-failed"
-            v-model="failed"
-        />
-</j-control-bar>
-<j-progress
-    :failed="failed"
-    :value="number"
-></j-progress>
 
-<!-- type ring -->
-<j-progress
-    :failed="failed"
-    :value="number"
-    :pause="pause"
-    ring
-></j-progress>
-
-<!--  type ring -->
-<j-button
-    @click="success = !success"
-    style="background: var(--foreground)"
->
-    <j-progress
-        ring
-        :failed="failed"
-        :value="number"
-        :pause="pause"
-    ></j-progress>
-    {{ number }} %
-</j-button>
                 `,
             intervalID: null,
             success: false,
