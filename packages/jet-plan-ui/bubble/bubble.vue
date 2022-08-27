708337994ae6f1f5d8db7e1f3f152a9cfcb277f3
abchiyi <meme_me2019@outlook.com>
2022-08-27 10:50:29 +0800

    [Change] 新增事件

--- packages/jet-plan-ui/bubble/bubble.vue
+++ packages/jet-plan-ui/bubble/bubble.vue
@@ -1,43 +1,44 @@
 
 <template>
-    <j-row style="display: inline-block">
-        <base-action
-            @hover_from="_showBubble"
-            @hover_to="hiddenBubble"
-            :class="classes"
-            ref="self"
-            v-re-screen-size="autoCheckPosition"
+    <base-action
+        @hover_from="start"
+        @hover_to="end"
+        :class="classes"
+        ref="self"
+        v-re-screen-size="autoCheckPosition"
+    >
+        <j-row
+            X="center"
+            Y="center"
         >
-            <j-row X="center" Y="center">
-                <slot></slot>
-                <transition-slide v-bind="positionMach.transitionSlide">
-                    <div
-                        v-show="showBubble"
-                        :class="bubbleClasses"
-                        class="font-mono frosted-glass"
-                        ref="bubble"
-                        tag="div"
-                        v-shadow:bottom="4"
-                    >
-                        {{ message }}
-                    </div>
-                </transition-slide>
-            </j-row>
-        </base-action>
-    </j-row>
+            <slot></slot>
+            <transition-slide v-bind="positionMach.transitionSlide">
+                <div
+                    v-show="showBubble"
+                    :class="bubbleClasses"
+                    class="font-mono frosted-glass"
+                    ref="bubble"
+                    tag="div"
+                    v-shadow:bottom="4"
+                >
+                    {{ message }}
+                </div>
+            </transition-slide>
+        </j-row>
+    </base-action>
 </template>
 
 <script>
-import baseAction from '../action-feedback/baseAction.vue';
-import { Row as JRow } from '../gird/index';
-import { Shadow, ReScreenSize } from '../tool/directives';
-import { propInit, validatorRange } from '../tool/lib';
-import { TransitionSlide } from '../animations';
-import { TimedActionLimit } from '../tool/lib';
-import { getOffset } from '../tool/lib/dom';
-const talEnter = new TimedActionLimit(400, 1);
-const talOut = new TimedActionLimit(50, 1);
-const Name = 'j-bubble';
+import baseAction from '../action-feedback/baseAction.vue'
+import { Row as JRow } from '../gird/index'
+import { Shadow, ReScreenSize } from '../tool/directives'
+import { propInit, validatorRange } from '../tool/lib'
+import { TransitionSlide } from '../animations'
+import { TimedActionLimit } from '../tool/lib'
+import { getOffset } from '../tool/lib/dom'
+const talEnter = new TimedActionLimit(400, 1)
+const talOut = new TimedActionLimit(50, 1)
+const Name = 'j-bubble'
 export default {
     name: Name,
     components: {
@@ -45,6 +46,7 @@ export default {
         baseAction,
         JRow,
     },
+    emits: ['bubbleShow'],
     props: {
         message: {
             required: true,
@@ -74,31 +76,31 @@ export default {
         },
     },
     computed: {
-        classes() {
-            return [Name];
+        classes () {
+            return [Name]
         },
-        bubbleClasses() {
+        bubbleClasses () {
             return [
                 'bubble',
                 this.positionMach.bubblePosition,
                 this.positionMach.start,
-            ];
+            ]
         },
-        positionMach() {
-            let rr = this.newPosition.split('-');
-            function reverse(str) {
+        positionMach () {
+            let rr = this.newPosition.split('-')
+            function reverse (str) {
                 // 设置参数以呈现元素从父元素中出现，而不是从外部飞入
                 switch (str) {
                     case 'top':
-                        return 'bottom';
+                        return 'bottom'
                     case 'bottom':
-                        return 'top';
+                        return 'top'
                     case 'left':
-                        return 'right';
+                        return 'right'
                     case 'right':
-                        return 'left';
+                        return 'left'
                     default:
-                        break;
+                        break
                 }
             }
             return {
@@ -107,87 +109,92 @@ export default {
                 transitionSlide: {
                     position: reverse(rr[0]),
                 },
-            };
+            }
         },
     },
 
-    created() {
-        this.showBubble = this.show;
+    created () {
+        this.showBubble = this.show
     },
-    data() {
+    data () {
         return {
             showBubble: false,
             showBubbleNow: false,
             nearEdgeOfScreen: undefined,
             newPosition: this.position,
-        };
+        }
     },
     methods: {
-        _showBubble(event) {
-            if (this.show != undefined) return;
-            this.autoCheckPosition(event.event.target);
-            this.edgeAvoidance();
-            talEnter.action(() => {});
-            this.showBubbleNow = true;
+        start () {
+            if (this.show != undefined) return
+            this._showBubble()
+        },
+        end () {
+            if (this.show != undefined) return
+            this.hiddenBubble()
+        },
+        _showBubble () {
+            this.autoCheckPosition(this.$refs.self)
+            this.edgeAvoidance()
+            talEnter.action(() => { })
+            this.showBubbleNow = true
             talEnter.setCooledAlarm(() => {
-                this.showBubble = this.showBubbleNow;
-            });
+                this.showBubble = this.showBubbleNow
+            })
         },
-        hiddenBubble() {
-            if (this.show != undefined) return;
-            talOut.action(() => {});
-            this.showBubbleNow = false;
+        hiddenBubble () {
+            talOut.action(() => { })
+            this.showBubbleNow = false
             talOut.setCooledAlarm(() => {
-                this.showBubble = this.showBubbleNow;
-            });
+                this.showBubble = this.showBubbleNow
+            })
         },
-        autoCheckPosition(el, screenSize) {
-            const marginMax = 50;
-            let elSize = getOffset(el);
-            screenSize = screenSize ? screenSize : ReScreenSize.reSize();
+        autoCheckPosition (el, screenSize) {
+            const marginMax = 50
+            let elSize = getOffset(el)
+            screenSize = screenSize ? screenSize : ReScreenSize.reSize()
             this.nearEdgeOfScreen = {
                 top: elSize.size.top <= marginMax,
                 left: elSize.size.left <= marginMax,
                 right: screenSize.width - elSize.size.right <= marginMax,
                 bottom: screenSize.height - elSize.size.bottom <= marginMax,
-            };
+            }
         },
-        edgeAvoidance() {
-            let rr = this.position.split('-');
-            let position = rr[0];
-            let start = rr[1];
-            function xStartTo(nearEdgeOfScreen) {
+        edgeAvoidance () {
+            let rr = this.position.split('-')
+            let position = rr[0]
+            let start = rr[1]
+            function xStartTo (nearEdgeOfScreen) {
                 if (nearEdgeOfScreen.left) {
-                    return nearEdgeOfScreen.right ? '' : 'start';
+                    return nearEdgeOfScreen.right ? '' : 'start'
                 } else if (nearEdgeOfScreen.right) {
-                    return 'end';
+                    return 'end'
                 } else if (nearEdgeOfScreen.top) {
-                    position = 'bottom';
-                    return '';
+                    position = 'bottom'
+                    return ''
                 }
-                return start;
+                return start
             }
             switch (position) {
                 case 'top':
-                    start = xStartTo(this.nearEdgeOfScreen);
-                    if (this.nearEdgeOfScreen.top) position = 'bottom';
-                    break;
+                    start = xStartTo(this.nearEdgeOfScreen)
+                    if (this.nearEdgeOfScreen.top) position = 'bottom'
+                    break
                 case 'bottom':
-                    start = xStartTo(this.nearEdgeOfScreen);
-                    if (this.nearEdgeOfScreen.bottom) position = 'top';
-                    break;
+                    start = xStartTo(this.nearEdgeOfScreen)
+                    if (this.nearEdgeOfScreen.bottom) position = 'top'
+                    break
                 case 'left':
-                    if (this.nearEdgeOfScreen.left) position = 'right';
-                    break;
+                    if (this.nearEdgeOfScreen.left) position = 'right'
+                    break
                 case 'right':
-                    if (this.nearEdgeOfScreen.right) position = 'left';
-                    break;
+                    if (this.nearEdgeOfScreen.right) position = 'left'
+                    break
                 default:
-                    break;
+                    break
             }
-            this.newPosition = `${position}${start ? '-' : ''}${
-                start ? start : ''
-            }`;
+            this.newPosition = `${position}${start ? '-' : ''}${start ? start : ''
+                }`
         },
     },
     directives: {
@@ -195,11 +202,16 @@ export default {
         ReScreenSize,
     },
     watch: {
-        show(v) {
-            this.showBubble = v;
+        show (v) {
+            if (v) {
+                this._showBubble()
+                this.$emit('bubbleShow', this.$refs.bubble)
+            } else {
+                this.hiddenBubble()
+            }
         },
     },
-};
+}
 </script>
 
 <style>
@@ -209,7 +221,7 @@ export default {
 }
 
 .j-bubble .bubble {
-    transition: all 0.3s var(--ease-out);
+    /* transition: all 0.3s var(--ease-out); */
     border-radius: var(--m-radius);
     color: var(--base-text-color);
     box-sizing: border-box;
@@ -227,6 +239,7 @@ export default {
 .j-bubble .top.start {
     left: 0;
 }
+
 .j-bubble .bottom.end,
 .j-bubble .top.end {
     right: 0;
@@ -236,6 +249,7 @@ export default {
     bottom: 100%;
     margin-bottom: 5px;
 }
+
 .j-bubble .bottom {
     top: 100%;
     margin-top: 5px;
@@ -267,6 +281,7 @@ export default {
 .j-bubble .right.j-transition-slide-leave-active {
     transform: translate3d(-40px, 0, 0);
 }
+
 .j-bubble .left.j-transition-slide-enter-from,
 .j-bubble .left.j-transition-slide-leave-active {
     transform: translate3d(40px, 0, 0);
